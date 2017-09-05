8abe12d7159fce52e15217baaba5923a04a4e714
sjorford <sjorford@gmail.com>
2017-09-05 17:45:07 +0100

    +overlay

--- proboards-tweaks.user.js
+++ proboards-tweaks.user.js
@@ -2,14 +2,16 @@
 // @name        Proboards tweaks
 // @namespace   sjorford@gmail.com
 // @include     http://vote-2012.proboards.com/*
-// @version     2017-09-05
+// @version     2017-09-05b
 // @grant       none
 // ==/UserScript==
 
 $(function() {
 	
 	$(`<style>
-		div.ui-form-error + img + style + div[id$="-banner"] {display: none !important;}
+		div.ui-form-error + img + style + div[id$="-banner"] 
+		div.ui-dialog.ui-widget.ui-widget-content + img + style + div[id$="-overlay"] 
+			{display: none !important;}
 	</style>`).appendTo('head');
 	
 });
