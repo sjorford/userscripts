21c2b47b37d5eeed8df84c0aff3fc2cf5f3c02be
sjorford <sjorford@gmail.com>
2017-09-10 09:28:55 +0100

    split out bulk adding

--- democracy-club-tweaks.user.js
+++ democracy-club-tweaks.user.js
@@ -9,15 +9,15 @@
 // @require     https://raw.githubusercontent.com/sjorford/js/master/sjo-jq.js
 // @require     https://raw.githubusercontent.com/sjorford/fun-with-elections/master/dc-lib.js
 // @require     https://raw.githubusercontent.com/sjorford/js/master/diff-string.js
+// @require     https://raw.githubusercontent.com/sjorford/userscripts/master/democracyclub/democlub-utils.js
 // ==/UserScript==
 
 // Restore correct line numbers in console log
-var console = Raven && Raven.p && Raven.p.log ? Raven.p : console;
+//var console = Raven && Raven.p && Raven.p.log ? Raven.p : console;
 
 // Parameters
 var rootUrl = 'https://candidates.democracyclub.org.uk/';
 var maxUrlLength = 40;
-var knownPartiesOnly = true;
 
 // Styles
 $(`<style>
@@ -91,17 +91,8 @@ $(`<style>
 	p {margin-bottom: 0.5rem;}
 	.header__nav {padding: 1em 0 0 0;}
 	h2 {font-size: 1.5rem;}
-	.sjo-bulkadd-listitem {list-style-type: none; margin-top: 0.5rem;}
-	.sjo-bulkadd-selected {background-color: #fff1a3;}
-	.sjo-bulkadd-listitem input {margin-bottom: 0 !important}
-	.sjo-bulkadd-data {font-size: 0.75rem; xxxcolor: #aaa; margin-bottom: 0rem !important; list-style-type: none;}
-	.sjo-bulkadd-link {font-weight: bold;}
 	
 	.show-new-candidate-form {display: none;}
-	
-	.form_group h3 {display: none !important;}
-	.form_group p {display: inline-block !important; vertical-align: top !important; width: 48% !important; padding-right: 1%; margin-bottom: 0 !important;}
-	.form_group p input {margin-bottom: 0.5rem !important;}
 	
 	.select2-result-label {font-size: 0.8rem;}
 	.person__actions__action {padding: 1em; margin-bottom: 1em;}
@@ -145,7 +136,7 @@ $(function() {
 	} else if (url.indexOf(rootUrl + 'bulk_adding/') === 0 && url.indexOf('/review/') >= 0) {
 		formatBulkAddReviewPage();
 	} else if (url.indexOf(rootUrl + 'bulk_adding/') === 0 && url.indexOf('/review/') < 0) {
-		formatBulkAddPage();
+		//formatBulkAddPage();
 	} else if (url == rootUrl + 'numbers/') {
 		formatStatistics();
 	} else if (url.indexOf(rootUrl + 'recent-changes') === 0) {
@@ -1051,65 +1042,6 @@ function formatEditForm() {
 // ================================================================
 // Bulk adding pages
 // ================================================================
-
-function formatPartySelects(selector) {
-	
-	var original = $('#sjo-party-select-original');
-	if (original.length == 0) {
-		original = $('<select id="sjo-party-select-original"></select>').appendTo('body');
-	}
-
-	var trimmed = $('#sjo-party-select-trimmed');
-	if (trimmed.length == 0) {
-		trimmed = $('<select id="sjo-party-select-trimmed"></select>').appendTo('body');
-	}
-	
-	var selects = $(selector);
-	var html = selects.eq(0).html();
-	
-	original.html(html);
-	
-	html = html.replace(/\((\d+) candidates\)/g, '[$1]');
-	if (knownPartiesOnly) {
-		html = html.replace(/<optgroup label="(.*?)">[^<]+(<option value="\d+">)[^<]+(<\/option>)[\s\S]*?<\/optgroup>/g, '$2$1$3');
-		html = html.replace(/<option value="\d+">[^<\[\]]+<\/option>/g, '');
-	}
-	
-	trimmed.html(html);
-	selects.html(html);
-
-}
-
-function formatBulkAddPage() {
-	
-	// TODO: allow party dropdowns to be deselected
-	
-	// Trim party selection
-	// TODO: make this a function
-	// TODO: allow deleted parties to be re-added
-	formatPartySelects('select.party-select');
-	
-	// Add a checkbox for reversed names
-	// TODO: add this to other edit pages
-	$('<input type="checkbox" id="sjo-reverse" value="reverse" checked><label for="sjo-reverse">Surname first</label>').insertBefore('#bulk_add_form').wrapAll('<div></div>');
-	
-	// Add a checkbox to show all parties
-	$('<input type="checkbox" id="sjo-allparties" value="allparties"><label for="sjo-allparties">Show all parties</label>').insertBefore('#bulk_add_form').wrapAll('<div></div>').change(showAllParties);
-	
-	// Hide noobstructions
-	var heading = $('h3:contains("How to add or check candidates")');
-	heading.next('ol').addClass('sjo-bulkadd-instructions').hide();
-	$('<a role="button" style="font-size: small;">Show</a>').click(event => $('.sjo-bulkadd-instructions').toggle()).appendTo(heading).before(' ');
-	
-}
-
-function showAllParties() {
-	
-	var checked = $('#sjo-allparties').prop('checked');
-	var source = $(checked ? '#sjo-party-select-original' : '#sjo-party-select-trimmed');
-	$('select.party-select').html(source.html());
-	
-}
 
 function formatBulkAddReviewPage() {
 	
