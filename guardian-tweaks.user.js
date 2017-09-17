d832c5e8ad475fc2b88151d8a0398f8a335b1ec3
sjorford <sjorford@gmail.com>
2017-09-17 09:49:46 +0100

    and that's all the weather

--- guardian-tweaks.user.js
+++ guardian-tweaks.user.js
@@ -4,8 +4,8 @@
 // @namespace   sjorford@gmail.com
 // @include     http://www.theguardian.com/*
 // @include     https://www.theguardian.com/*
-// @version     2017-09-12
-// @grant       unsafeWindow
+// @version     2017-09-17
+// @grant       none
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
 // ==/UserScript==
 
@@ -14,8 +14,12 @@ $(function() {
 	$(`<style>
 		#comments {display: none;}
 		.live-blog__sticky-components-container.affix {position: static;}
+		.weather {display: none;}
 	</style>`).appendTo('head');
 	
-	$('.fc-item__byline').filter(':contains("Suzanne Moore"), :contains("Zoe Williams"), :contains("Tim Dowling")').closest('.fc-item').hide();
+	$('.fc-item__byline').filter(`
+		:contains("Suzanne Moore"), 
+		:contains("Zoe Williams"), 
+		:contains("Tim Dowling")`).closest('.fc-item').hide();
 	
 });
