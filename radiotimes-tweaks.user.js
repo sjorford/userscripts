96e49f56f94e0d7ae9ef5971ea688613c910fa44
sjorford <sjorford@gmail.com>
2017-09-26 15:26:53 +0100

    no

---
+++ radiotimes-tweaks.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name        Radio Times tweaks
+// @namespace   sjorford@gmail.com
+// @include     http://www.radiotimes.com/*
+// @version     2017-09-26
+// @grant       none
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
+// ==/UserScript==
+
+$(function() {
+	
+	$(`<style>
+		
+		link[href*="d7f3fb657c"] + script + script + script + script + div,
+		link[href*="d7f3fb657c"] + script + script + script + script + div + div,
+		
+		link[href*="d7f3fb657c"] + div,
+		link[href*="d7f3fb657c"] + div + div
+
+			{display: none !important;}
+			
+	</style>`).appendTo('head');
+	
+});
