e5345b7b0d4120c7cc3e2af663f4764772572ab4
sjorford <sjorford@gmail.com>
2017-10-04 21:23:33 +0100

    list in columns

--- democracyclub/democlub-election-tweaks.user.js
+++ democracyclub/democlub-election-tweaks.user.js
@@ -2,7 +2,7 @@
 // @name        Demo Club Every Election tweaks
 // @namespace   sjorford@gmail.com
 // @include     https://elections.democracyclub.org.uk/*
-// @version     2017-10-03
+// @version     2017-10-04
 // @grant       none
 // @require     https://code.jquery.com/jquery-3.2.1.min.js
 // @require     https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js
@@ -10,19 +10,21 @@
 
 $(`<style>
 	.sjo-polldate {margin: 2.5em 1em 0 0; vertical-align: bottom;}
+	.block-label {background: inherit; border: none; margin: 0; padding: 0 0 0 30px; float: none;}
+	.block-label input {top: -2px; left: 0;}
+	.sjo-columns {column-width: 15em;}
 </style>`).appendTo('head');
 
 $(function() {
 	
+	// Display buttons for Thursdays
 	var wrapper = $('<div></div>').insertAfter('.form-date');
-	
 	var thursday = moment().isoWeekday(4);
-	console.log(thursday);
-	
 	for (var i = 0; i < 5; i++) {
 		$(`<input class="sjo-polldate" type="button" value="${thursday.add(7, 'days').format('YYYY-MM-DD')}">`).appendTo(wrapper);
 	}
 	
+	// Fill in date fields
 	$('body').on('click', '.sjo-polldate', (event) => {
 		var date = moment(event.target.value);
 		console.log(date);
@@ -30,5 +32,14 @@ $(function() {
 		$('.form-group-month input').val(date.format('M'));
 		$('.form-group-day input').val(date.format('D'));
 	});
+	
+	// Trim council names
+	var labels = $('.block-label');
+	var labelElements = labels.contents()
+		.filter((index, element) => element.nodeType == 3)
+		.each((index, element) => element.nodeValue = element.nodeValue.trim().replace(/^(Borough of |Borough Council of |London Borough of |Royal Borough of |City of |City and County of |Council of the |Comhairle nan )?(.+?)(( County| County Borough| Metropolitan Borough| Borough| Metropolitan District| District| City and District| City)? Council)?$/, '$2'))
+		.closest('.block-label').toArray()
+		.sort((a, b) => a.innerText > b.innerText);
+	labels.first().parent().addClass('sjo-columns').append(labelElements);
 	
 });
