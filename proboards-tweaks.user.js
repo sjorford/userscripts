5802321f5116bce60b6c1629a7d3990aeaff0f1d
sjorford <sjorford@gmail.com>
2017-11-06 15:38:44 +0000

    require jQuery

--- proboards-tweaks.user.js
+++ proboards-tweaks.user.js
@@ -2,8 +2,9 @@
 // @name        Proboards tweaks
 // @namespace   sjorford@gmail.com
 // @include     http://vote-2012.proboards.com/*
-// @version     2017-09-17
+// @version     2017-11-06
 // @grant       none
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
 // ==/UserScript==
 
 $(function() {
