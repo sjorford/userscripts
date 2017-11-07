ac935fa47a049292dc40c355226477129ad4f365
sjorford <sjorford@gmail.com>
2017-11-07 17:01:59 +0000

    display sub-IDs

--- democracyclub/democlub-election-tweaks.user.js
+++ democracyclub/democlub-election-tweaks.user.js
@@ -2,13 +2,15 @@
 // @name        Demo Club Every Election tweaks
 // @namespace   sjorford@gmail.com
 // @include     https://elections.democracyclub.org.uk/*
-// @version     2017-11-05
+// @version     2017-11-07
 // @grant       none
 // @require     https://code.jquery.com/jquery-3.2.1.min.js
 // @require     https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js
+// @require     https://raw.githubusercontent.com/sjorford/userscripts/master/democracyclub/democlub-utils.js
 // ==/UserScript==
 
 $(`<style>
+	.sjo-election-sublist {font-size: 10pt;}
 	.sjo-polldate {margin: 2.5em 0.5em 0 0; vertical-align: bottom;}
 	.block-label {background: inherit; border: none; margin: 0; padding: 0 0 0 30px; float: none;}
 	.block-label input {top: -2px; left: 0;}
@@ -20,6 +22,44 @@ $(function() {
 	
 	// Add link to radar
 	$('.menu').append('<li><a href="/election_radar/?status=new">Radar</a></li>');
+	
+	// Display sub-IDs on front page
+	var elections = {};
+	$('.card h3:contains("Upcoming elections") + ul li').addClass('sjo-election-top').each((index, element) => {
+		
+		var key = $('a', element).attr('href').match(/\/elections\/([^\/]+)\//)[1];
+		var date = key.split('.')[1];
+		if (date == '2018-05-03') return; // TODO
+		
+		// Get data for this key
+		if (!elections[date]) {
+			elections[date] = {};
+			$.getJSON('https://elections.democracyclub.org.uk/api/elections.json?poll_open_date=' + date, data => {
+				
+				$.each(data.results, (index, element) => elections[date][element.election_id] = element);
+				
+				$.each(elections[date], (index, element) => {
+					var top = $('.sjo-election-top > a[href*="' + element.election_id + '"]').closest('li');
+					if (top.length == 0) {
+						return;
+					}
+					var list = $('<ul class="sjo-election-sublist"></ul>').appendTo(top);
+					populateList(element);
+					
+					function populateList(election) {
+						if (election.children.length > 0) {
+							$.each(election.children, (index, element) => populateList(elections[date][element])); // why yes, this _is_ the fourth time I have declared (index, element) in this script, thank you for noticing
+						} else {
+							$(`<li><a href="/elections/${election.election_id}/">${election.election_id}</a></li>`).appendTo(list);
+						}
+					}
+					
+				});
+				
+			});
+		}
+		
+	});
 	
 	// Display buttons for Thursdays
 	var wrapper = $('<div></div>').insertAfter('.form-date');
