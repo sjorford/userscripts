438acd1ac609301dc5fd80dbb5c989dc4cd20cdd
sjorford <sjorford@gmail.com>
2017-11-13 13:10:01 +0000

    +calendar, split functions

--- democracyclub/democlub-election-tweaks.user.js
+++ democracyclub/democlub-election-tweaks.user.js
@@ -2,16 +2,20 @@
 // @name        Demo Club Every Election tweaks
 // @namespace   sjorford@gmail.com
 // @include     https://elections.democracyclub.org.uk/*
-// @version     2017-11-07
+// @version     2017-11-13
 // @grant       none
 // @require     https://code.jquery.com/jquery-3.2.1.min.js
+// @require     https://code.jquery.com/ui/1.12.1/jquery-ui.min.js
 // @require     https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js
 // @require     https://raw.githubusercontent.com/sjorford/userscripts/master/democracyclub/democlub-utils.js
 // ==/UserScript==
 
+console.log(1);
+
 $(`<style>
 	.sjo-election-sublist {font-size: 10pt;}
-	.sjo-polldate {margin: 2.5em 0.5em 0 0; vertical-align: bottom;}
+	.sjo-date-picker {clear: both; margin-bottom: 1em;}
+	.sjo-date-normal a {background: #7eeab5 !important;}
 	.block-label {background: inherit; border: none; margin: 0; padding: 0 0 0 30px; float: none;}
 	.block-label input {top: -2px; left: 0;}
 	.sjo-columns {column-width: 15em;}
@@ -22,6 +26,22 @@ $(function() {
 	
 	// Add link to radar
 	$('.menu').append('<li><a href="/election_radar/?status=new">Radar</a></li>');
+	
+	if (location.href == 'https://elections.democracyclub.org.uk/') {
+		displaySubIDs();
+	}
+	
+	if (location.href.indexOf('id_creator/date/') >= 0) {
+		displayDatePicker();
+	}
+	
+	if (location.href.indexOf('id_creator/election_organisation/') >= 0) {
+		trimCouncilNames();
+	}
+	
+});
+
+function displaySubIDs() {
 	
 	// Display sub-IDs on front page
 	var elections = {};
@@ -61,34 +81,48 @@ $(function() {
 		
 	});
 	
-	// Display buttons for Thursdays
-	var wrapper = $('<div></div>').insertAfter('.form-date');
-	var thursday = moment().isoWeekday(4);
-	for (var i = 0; i < 6; i++) {
-		$(`<input class="sjo-polldate" type="button" value="${thursday.add(7, 'days').format('YYYY-MM-DD')}">`).appendTo(wrapper);
-	}
+}
+
+function displayDatePicker() {
 	
-	// Fill in date fields
-	$('body').on('click', '.sjo-polldate', (event) => {
-		var date = moment(event.target.value);
-		console.log(date);
-		$('.form-group-year input').val(date.format('YYYY'));
-		$('.form-group-month input').val(date.format('M'));
-		$('.form-group-day input').val(date.format('D'));
+	$('<link href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css" rel="stylesheet" type="text/css">').appendTo('head');
+	
+	var today = moment();
+	
+	var wrapper = $('<div class="sjo-date-picker"></div>').insertAfter('.form-date').wrap('<div></div>');
+	wrapper.datepicker({
+		dateFormat: 'yy-mm-dd',
+		showOtherMonths: true,
+		selectOtherMonths: true,
+		beforeShowDay: date => {
+			var _date = moment(date);
+			return [true, _date.day() == 4 && _date.isAfter(today) ? 'sjo-date-normal' : '', ''];
+		},
+		onSelect: dateText => {
+			var date = moment(dateText);
+			console.log(date);
+			$('.form-group-year input').val(date.format('YYYY'));
+			$('.form-group-month input').val(date.format('M'));
+			$('.form-group-day input').val(date.format('D'));
+		},
 	});
 	
-	// Trim council names
-	if (location.href.indexOf('id_creator/election_organisation/') >= 0) {
-		var oldNICouncils = ['Antrim', 'Ards', 'Armagh', 'Ballymena', 'Ballymoney', 'Banbridge', 'Carrickfergus', 'Castlereagh', 'Coleraine', 'Cookstown', 'Craigavon', 'Derry', 'Down', 'Dungannon and South Tyrone', 'Fermanagh', 'Larne', 'Limavady', 'Lisburn', 'Magherafelt', 'Moyle', 'Newry and Mourne', 'Newtownabbey', 'North Down', 'Omagh', 'Strabane'];
-		var labels = $('.block-label');
-		labels.contents()
-			.filter((index, element) => element.nodeType == 3)
-			.each((index, element) => {
-				var council = Utils.shortOrgName(element.nodeValue);
-				if (oldNICouncils.indexOf(council) >= 0) $(element).closest('.block-label').addClass('sjo-obsolete');
-				element.nodeValue = council;
-			})
-		labels.first().parent().addClass('sjo-columns').append(labels.toArray().sort((a, b) => a.innerText > b.innerText));
-	}
+}
+
+function trimCouncilNames() {
 	
-});
+	// TODO: put these in a config file
+	var oldNICouncils = ['Antrim', 'Ards', 'Armagh', 'Ballymena', 'Ballymoney', 'Banbridge', 'Carrickfergus', 'Castlereagh', 'Coleraine', 'Cookstown', 'Craigavon', 'Derry', 'Down', 'Dungannon and South Tyrone', 'Fermanagh', 'Larne', 'Limavady', 'Lisburn', 'Magherafelt', 'Moyle', 'Newry and Mourne', 'Newtownabbey', 'North Down', 'Omagh', 'Strabane'];
+	
+	// Trim council names
+	var labels = $('.block-label');
+	labels.contents()
+		.filter((index, element) => element.nodeType == 3)
+		.each((index, element) => {
+			var council = Utils.shortOrgName(element.nodeValue);
+			if (oldNICouncils.indexOf(council) >= 0) $(element).closest('.block-label').addClass('sjo-obsolete');
+			element.nodeValue = council;
+		});
+	labels.first().parent().addClass('sjo-columns').append(labels.toArray().sort((a, b) => a.innerText > b.innerText));
+
+}
