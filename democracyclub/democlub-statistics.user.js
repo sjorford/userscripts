1f600e0e17eff1abacf37576142183b4117a1946
sjorford <sjorford@gmail.com>
2017-11-27 16:10:48 +0000

    bump version

--- democracyclub/democlub-statistics.user.js
+++ democracyclub/democlub-statistics.user.js
@@ -2,7 +2,7 @@
 // @name        Demo Club format statistics
 // @namespace   sjorford@gmail.com
 // @include     https://candidates.democracyclub.org.uk/numbers/
-// @version     2017-11-15
+// @version     2017-11-27
 // @grant       none
 // @require     https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js
 // @require     https://raw.githubusercontent.com/sjorford/js/master/sjo-jq.js
