dd8abededb1ae01b683d010cd06e28188e44a922
sjorford <sjorford@gmail.com>
2018-01-05 22:29:53 +0000

    -banner

--- proboards-tweaks.user.js
+++ proboards-tweaks.user.js
@@ -2,7 +2,7 @@
 // @name        Proboards tweaks
 // @namespace   sjorford@gmail.com
 // @include     http://vote-2012.proboards.com/*
-// @version     2017-11-30
+// @version     2018-01-05
 // @grant       none
 // ==/UserScript==
 
@@ -12,6 +12,7 @@ $(function() {
 		div.ui-form-error + img + style + div[id$="-banner"],
 		iframe#rufous-sandbox   + style + div[id$="-banner"],
 		div.ui-dialog.ui-widget.ui-widget-content + img + style + div[id$="-overlay"],
+		div.ui-dialog.ui-widget.ui-widget-content + img + style + div[id$="-banner"],
 		div#support-message
 			{display: none !important;}
 		tr.bookmarked.new {background-color: #ffd700b3 !important;}
