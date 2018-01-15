255ccb017be8099ef569948c981ab1d88d339b46
sjorford <sjorford@gmail.com>
2018-01-15 12:32:42 +0000

    slight DOM change

--- proboards-tweaks.user.js
+++ proboards-tweaks.user.js
@@ -2,21 +2,25 @@
 // @name        Proboards tweaks
 // @namespace   sjorford@gmail.com
 // @include     http://vote-2012.proboards.com/*
-// @version     2018-01-06
+// @version     2018-01-15
 // @grant       none
 // ==/UserScript==
 
 $(function() {
 	
 	$(`<style>
-		div.ui-form-error + img + style + div[id$="-banner"],
-		iframe#rufous-sandbox   + style + div[id$="-banner"],
+		
+		div.ui-form-error     + img + style + div[id$="-banner"],
+		iframe#rufous-sandbox       + style + div[id$="-banner"],
+		iframe#rufous-sandbox + img + style + div[id$="-banner"],
 		div.ui-dialog.ui-widget.ui-widget-content + img + style + div[id$="-overlay"],
 		div.ui-dialog.ui-widget.ui-widget-content + img + style + div[id$="-banner"],
 		div#support-message
 			{display: none !important;}
+		
 		tr.bookmarked.new {background-color: #ffd700b3 !important;}
-		tr.new {background-color: #ffd70066 !important}
+		tr.new            {background-color: #ffd70066 !important;}
+		
 	</style>`).appendTo('head');
 	
 });
