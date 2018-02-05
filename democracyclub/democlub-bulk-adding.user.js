06177ed60ddb894987de5552fc8f0ae995b5977f
sjorford <sjorford@gmail.com>
2018-02-05 06:59:33 +0000

    show rows incrementally

--- democracyclub/democlub-bulk-adding.user.js
+++ democracyclub/democlub-bulk-adding.user.js
@@ -3,7 +3,7 @@
 // @namespace   sjorford@gmail.com
 // @include     https://candidates.democracyclub.org.uk/bulk_adding/*
 // @exclude     https://candidates.democracyclub.org.uk/bulk_adding/*/review/
-// @version     2018.02.02
+// @version     2018.02.05
 // @grant       none
 // @require     https://raw.githubusercontent.com/sjorford/userscripts/master/democracyclub/democlub-utils.js
 // ==/UserScript==
@@ -15,11 +15,10 @@ window.setTimeout(onready, 0);
 function onready() {
 	
 	$(`<style>
-		
-		.form_group h3 {display: none !important;}
-		.form_group p {display: inline-block !important; vertical-align: top !important; width: 48% !important; padding-right: 1%; margin-bottom: 0 !important;}
-		.form_group p input {margin-bottom: 0.5rem !important;}
-		
+		#bulk_add_form .form_group {display: none;}
+		#bulk_add_form .form_group h3 {display: none;}
+		#bulk_add_form .form_group p {display: inline-block; vertical-align: top; width: 48%; padding-right: 1%; margin-bottom: 0;}
+		#bulk_add_form .form_group p input {margin-bottom: 0.5rem;}
 	</style>`).appendTo('head');
 	
 	// TODO: allow party dropdowns to be deselected
@@ -33,6 +32,10 @@ function onready() {
 	
 	// Add a checkbox to show all parties
 	$('<input type="checkbox" id="sjo-allparties" value="allparties"><label for="sjo-allparties">Show all parties</label>').insertBefore('#bulk_add_form').wrapAll('<div></div>').change(Utils.showAllParties);
+	
+	// Show rows incrementally
+	$('#bulk_add_form .form_group').first().show();
+	$('#bulk_add_form input').on('change', event => $(event.target).closest('.form_group').next('.form_group').show());
 	
 	// Hide noobstructions
 	var heading = $('h3:contains("How to add or check candidates")');
