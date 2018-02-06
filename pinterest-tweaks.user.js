d19068b492e0e786fe3396545814b8a7f2adbcf0
sjorford <sjorford@gmail.com>
2018-02-06 13:26:50 +0000

    fuck off

---
+++ pinterest-tweaks.user.js
@@ -0,0 +1,15 @@
+﻿// ==UserScript==
+// @name           Pinterest tweaks
+// @namespace      sjorford@gmail.com
+// @author         Stuart Orford
+// @version        2018-02-06
+// @match          https://www.pinterest.co.uk/*
+// @grant          none
+// @require        https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
+// ==/UserScript==
+
+$('<style>[data-test-id="giftWrap"] {display: none;}</style>').appendTo('head');
+
+$(function() {
+	
+});
