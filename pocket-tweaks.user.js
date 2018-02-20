364201e7f743032e0261d052abb538fc1225ed00
sjorford <sjorford@gmail.com>
2018-02-20 18:22:32 +0000

    hide crap

---
+++ pocket-tweaks.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Pocket tweaks
+// @namespace    sjorford@gmail.com
+// @version      2018-02-20
+// @match        https://getpocket.com/a/queue/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    $('<style>.item_type_spoc {display: none;}</style>').appendTo('head');
+})();
