19998ac336dd145410c9fb4073799a21c6c20edd
Stuart Orford <sjorford@gmail.com>
2018-04-30 09:44:05 +0100

    hide wtf

--- twitter-tweaks.user.js
+++ twitter-tweaks.user.js
@@ -2,7 +2,7 @@
 // @name        Twitter tweaks
 // @namespace   sjorford@gmail.com
 // @include     https://twitter.com/*
-// @version     2018.04.20.0
+// @version     2018.04.30.0
 // @grant       none
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
 // @require     https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/moment.min.js
@@ -36,6 +36,8 @@ $(`<style>
 	li[data-suggestion-json*="ActivityTweet"] {display: none;}
 
 	.tweet.muting {display: none;}
+	
+	.wtf-module {display: none !important;}
 	
 </style>`).appendTo('head');
 
