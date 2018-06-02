113de31736315cc0d9d36c75ad628b8cb4912da8
sjorford <sjorford@gmail.com>
2018-06-02 12:56:37 +0100

    moved to sidebar script

--- twitter-tweaks.user.js
+++ twitter-tweaks.user.js
@@ -2,7 +2,7 @@
 // @name        Twitter tweaks
 // @namespace   sjorford@gmail.com
 // @include     https://twitter.com/*
-// @version     2018.05.02.0
+// @version     2018.06.02.0
 // @grant       none
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
 // @require     https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/moment.min.js
@@ -43,75 +43,5 @@ $(`<style>
 </style>`).appendTo('head');
 
 $(function() {
-	
-	var dateFormatString = 'YYYY-MM-DD HH:mm:ss';
-	var target;
-	var lists;
-	
-	// TODO: keep this running so that it reloads when internal links rewrite the page?
-	//checkForDashboard();
-	
-	function checkForDashboard() {
-		
-		target = $('.dashboard .module');
-		if (target.length == 0) {
-			setTimeout(checkForDashboard, 1000);
-		} else {
-			getListOfLists();
-		}
-		
-	}
-	
-	function getListOfLists() {
-		
-		lists = localStorage.getItem('sjo-twitter-lists');
-		var lastChecked = localStorage.getItem('sjo-twitter-lists-update');
-		if (!lists || !lastChecked || moment(lastChecked, dateFormatString).isBefore(moment().subtract(1, 'days'))) {
-			refreshLists();
-		} else {
-			lists = JSON.parse(lists);
-			addListsModule();
-		}
-		
-	}
-	
-	function refreshLists() {
-		
-		console.log('refreshing list of lists');
-		$.get('https://twitter.com/sjorford/lists/', data => {
-			lists = [];
-			var regex = /<a class="[^"]*ProfileListItem-name[^"]*"[^<]*href="\/[^\/]+\/lists\/[^"]+">([^"]+)<\/a>/g;
-			var matches;
-			while (matches = regex.exec(data)) {
-				lists.push(matches[1]);
-			}
-			console.log('lists found:' + lists.join(', '));
-			localStorage.setItem('sjo-twitter-lists', JSON.stringify(lists));
-			localStorage.setItem('sjo-twitter-lists-update', moment().format(dateFormatString));
-			addListsModule();
-		});
-		
-	}
-	
-	function addListsModule() {
-		
-		var module = $('<div class="module"></div>').insertAfter(target.first());
-		var flexModule = $('<div class="flex-module"></div>').appendTo(module);
-		var flexModuleHeader = $('<div class="flex-module-header"></div>').appendTo(flexModule)
-			.append('<h3>Lists</h3>')
-			.append('<small> · <button type="button" class="btn-link sjo-lists-refresh">Refresh</button></small>')
-			.append('<small> · <a href="/sjorford/lists/">View all</a></small>');
-		var flexModuleInner = $('<div class="flex-module-inner"></div>').appendTo(flexModule);
-		
-		$.each(lists.sort((a, b) => a > b), (index, value) => {
-			flexModuleInner.append(`<a class="sjo-list-link" href="/sjorford/lists/${value.toLowerCase().replace(/ /g, '-')}">${value}</a>`);
-		});
-		
-		$('#sjo-lists-refresh').click(() => {
-			refreshLists();
-			return false;
-		});
-		
-	}
 	
 });
