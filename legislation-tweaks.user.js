38af55eda95ccaaff94b7cb1aa739cab6c7f2e57
sjorford <sjorford@gmail.com>
2018-10-29 06:55:11 +0000

    url + older changes?

--- legislation-tweaks.user.js
+++ legislation-tweaks.user.js
@@ -2,8 +2,8 @@
 // @name           Legislation.gov.uk tweaks
 // @namespace      sjorford@gmail.com
 // @author         Stuart Orford
-// @version        2018.10.29.0
-// @match          http://www.legislation.gov.uk/all?*
+// @version        2018.10.29.1
+// @match          http://www.legislation.gov.uk/*
 // @match          file:///C:/Users/stuarto/Google%20Drive/Personal/Politics/ECOs/*
 // @match          file:///C:/Users/Stuart/Google%20Drive/Personal/Politics/ECOs/*
 // @grant          none
@@ -12,7 +12,7 @@
 $(function() {
 	
 	// Repopulate search box
-	var locationMatch = window.location.href.match(/\/all\?title=([^&]+)/);
+	var locationMatch = window.location.href.match(/\?title=([^&]+)/);
 	if (locationMatch) {
 		$('#title').val(decodeURIComponent(locationMatch[1]));
 	}
@@ -29,11 +29,12 @@ $(function() {
 		var cells = row.find('.LegText').closest('td');
 		cells.each((cellIndex, cellElement) => {
 			
-			$(cellElement).find('p').each((paraIndex, paraElement) => {
+			var paras = $(cellElement).find('p');
+			paras.each((paraIndex, paraElement) => {
 				if (paraElement == null) return;
 				
 				var para = $(paraElement);
-				var newCell = $('<td class="LegTD"></td>').html(para.html());
+				var newCell = $('<td class="LegTD"></td>').html(para.html().trim());
 				
 				if (cellIndex == 0) {
 					lastRow = $('<tr class="sjo-row"></tr>').insertAfter(lastRow);
@@ -59,6 +60,7 @@ $(function() {
 		.filter((index, element) => $(element).closest('tr').find('td, th')
 			.filter((index, element) => $(element).text().trim() != '').length == 1)
 		.add('.LegTD strong, .LegTH strong').closest('tr');
+	rows.closest('table').css({maxWidth: '800px'}).find('colgroup').remove();
 	rows.each((index, element) => {
 		var row = $(element);
 		var district = row.text().trim().replace(/^(In the )?(city|borough|district) of |^In | (City|Borough|District):?$/ig, '');
@@ -68,6 +70,5 @@ $(function() {
 		followingRows.each((index, element) => $('<td class="LegTDplain"></td>').text(district).prependTo(element));
 		row.remove();
 	});
-	$('col[width="100%"]').remove();
 	
 });
