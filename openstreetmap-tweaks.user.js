a3be95aef90615a2516e9e120872eb386f701e03
sjorford <sjorford@gmail.com>
2018-12-11 07:44:32 +0000

    preset Address

--- openstreetmap-tweaks.user.js
+++ openstreetmap-tweaks.user.js
@@ -2,7 +2,7 @@
 // @name           OpenStreetMap tweaks
 // @namespace      sjorford@gmail.com
 // @author         Stuart Orford
-// @version        2018.12.01.0
+// @version        2018.12.11.0
 // @match          https://www.openstreetmap.org/id
 // @grant          none
 // @require        https://code.jquery.com/jquery-3.3.1.min.js
@@ -14,23 +14,28 @@ $(function() {
 	
 	$('body').on('keypress', event => {
 		var oe = event.originalEvent;
-		
-		// Change building type to house (Ctrl-H)
+		if (!$('body').is(oe.originalTarget)) return;
+
+		// Change area type to House (Ctrl-H)
 		if (oe.key == 'h' && oe.ctrlKey && !oe.shiftKey && !oe.altKey) {
 			event.preventDefault();
-			house();
-			address();
+			presetHouse();
+			enterAddress();
 			return false;
 		}
 		
-		// Enter address (Ctrl-A)
+		// Enter address, or change point type to Address (Ctrl-A)
 		if (oe.key == 'a' && oe.ctrlKey && !oe.shiftKey && !oe.altKey) {
 			event.preventDefault();
-			address();
+			if ($('g.node.point.selected').length > 0) {
+				presetAddress();
+			} else {
+				enterAddress();
+			}
 			return false;
 		}
 		
-		function house() {
+		function presetHouse() {
 			console.log('changing type: house');
 			$('.preset-reset.preset-choose').first().click();
 			$('.preset-list-item.preset-category-building button').first().click();
@@ -38,7 +43,18 @@ $(function() {
 				.closest('.preset-list-button').not('.preset-reset').first().click();
 		}
 		
-		function address() {
+		function presetAddress() {
+			console.log('changing type: address');
+			$('.preset-reset.preset-choose').first().click();
+			var button = $('.preset-icon-fill-vertex, .preset-icon-fill-point')
+					.filter((i, e) => e.className.split(' ').indexOf('tag-addr:*') >= 0)
+					.closest('.preset-list-button').not('.preset-reset').first();
+			if (button.length == 0) return;
+			button.click();
+			enterAddress();
+		}
+		
+		function enterAddress() {
 			console.log('moving focus to address fields');
 			$('.addr-housenumber').first().focus();
 		}
