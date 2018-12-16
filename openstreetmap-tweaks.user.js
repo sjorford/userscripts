0da0fcdf98c6ef4426bc8f42f33fc3f19cd4eae3
sjorford <sjorford@gmail.com>
2018-12-16 17:07:18 +0000

    presetGarage

--- openstreetmap-tweaks.user.js
+++ openstreetmap-tweaks.user.js
@@ -2,7 +2,7 @@
 // @name           OpenStreetMap tweaks
 // @namespace      sjorford@gmail.com
 // @author         Stuart Orford
-// @version        2018.12.15.1
+// @version        2018.12.16.0
 // @match          https://www.openstreetmap.org/id
 // @grant          none
 // @require        https://code.jquery.com/jquery-3.3.1.min.js
@@ -13,31 +13,47 @@ $(function() {
 	//var $ = $sjo;
 	
 	console.log('OpenStreetMap tweaks');
+
+	function presetHouse() {
+		console.log('changing type: house');
+		$('.preset-reset.preset-choose').first().click();
+		$('.preset-list-item.preset-category-building button').first().click();
+		$('.preset-icon-fill-area.tag-building-house')
+			.closest('.preset-list-button').not('.preset-reset').first().click();
+	}
+
+	function presetAddress() {
+		console.log('changing type: address');
+		$('.preset-reset.preset-choose').first().click();
+		var button = $('.preset-icon-fill-vertex, .preset-icon-fill-point')
+		.filter((i, e) => e.className.split(' ').indexOf('tag-addr:*') >= 0)
+		.closest('.preset-list-button').not('.preset-reset').first();
+		if (button.length == 0) return;
+		button.click();
+		enterAddress();
+	}
+
+	function enterAddress() {
+		console.log('moving focus to address fields');
+		$('.addr-housenumber').first().focus();
+	}
 	
-		function presetHouse() {
-			console.log('changing type: house');
-			$('.preset-reset.preset-choose').first().click();
-			$('.preset-list-item.preset-category-building button').first().click();
-			$('.preset-icon-fill-area.tag-building-house')
-				.closest('.preset-list-button').not('.preset-reset').first().click();
-		}
-		
-		function presetAddress() {
-			console.log('changing type: address');
-			$('.preset-reset.preset-choose').first().click();
-			var button = $('.preset-icon-fill-vertex, .preset-icon-fill-point')
-			.filter((i, e) => e.className.split(' ').indexOf('tag-addr:*') >= 0)
-			.closest('.preset-list-button').not('.preset-reset').first();
-			if (button.length == 0) return;
-			button.click();
-			enterAddress();
-		}
-		
-		function enterAddress() {
-			console.log('moving focus to address fields');
-			$('.addr-housenumber').first().focus();
-		}
-		
+	function presetGarage() {
+		console.log('changing type: garage');
+		$('.preset-reset.preset-choose').first().click();
+		$('.preset-list-item.preset-category-building button').first().click();
+		$('.preset-icon-fill-area.tag-building-garage')
+			.closest('.preset-list-button').not('.preset-reset').first().click();
+	}
+	
+	function presetGarages() {
+		console.log('changing type: garages');
+		$('.preset-reset.preset-choose').first().click();
+		$('.preset-list-item.preset-category-building button').first().click();
+		$('.preset-icon-fill-area.tag-building-garages')
+			.closest('.preset-list-button').not('.preset-reset').first().click();
+	}
+	
 	var hotkeys = [
 		
 		// Ctrl-A: convert node to address node, or enter the address of a building
@@ -54,6 +70,20 @@ $(function() {
 			if ($('.selected').is('path.area')) {
 				presetHouse();
 				enterAddress();
+			}
+		}},
+		
+		// Ctrl-G: convert area to garage
+		{key: 'g', ctrl: true, shift: false, alt: false, fn: () => {
+			if ($('.selected').is('path.area')) {
+				presetGarage();
+			}
+		}},
+		
+		// Ctrl-Shift-G: convert area to garages
+		{key: 'g', ctrl: true, shift: false, alt: false, fn: () => {
+			if ($('.selected').is('path.area')) {
+				presetGarages();
 			}
 		}},
 		
