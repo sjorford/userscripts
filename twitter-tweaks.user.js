6a69ef710e32f8745f3c83151a6fe05111ca0040
sjorford <sjorford@gmail.com>
2019-02-17 08:22:35 +0000

    strip unused code

--- twitter-tweaks.user.js
+++ twitter-tweaks.user.js
@@ -2,7 +2,7 @@
 // @name        Twitter tweaks
 // @namespace   sjorford@gmail.com
 // @include     https://twitter.com/*
-// @version     2018.10.31.0
+// @version     2019.02.17.0
 // @grant       none
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
 // @require     https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/moment.min.js
@@ -21,19 +21,11 @@ $(`<style>
 	.wtf-module {display: none !important;}
 	.module.trends {display: none !important;}
 	
-	.sjo-botuser * {display: none !important;}
-	
 </style>`).appendTo('head');
 
 $(function() {
 	
 	// Strip search parameters
 	if (window.location.search.match(/^\?(lang=en(-gb)?$|ref_src=)/)) window.location = window.location.href.split('?')[0];
-	
-	// Hide bots
-	function hide12345678() {
-		$('li').not('.sjo-botuser').find('a.account-group').filter((i,e) => e.href.match(/\d{8}$/)).closest('li').addClass('sjo-botuser');
-	}
-	window.setInterval(hide12345678, 50);
 	
 });
