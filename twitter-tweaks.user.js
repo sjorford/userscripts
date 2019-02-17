70679cc6eace905de4704cc1fa6b64d4b209493a
sjorford <sjorford@gmail.com>
2019-02-17 08:27:38 +0000

    Shift-Alt-F focuses search box

--- twitter-tweaks.user.js
+++ twitter-tweaks.user.js
@@ -2,7 +2,7 @@
 // @name        Twitter tweaks
 // @namespace   sjorford@gmail.com
 // @include     https://twitter.com/*
-// @version     2019.02.17.0
+// @version     2019.02.17.1
 // @grant       none
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
 // @require     https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/moment.min.js
@@ -27,5 +27,14 @@ $(function() {
 	
 	// Strip search parameters
 	if (window.location.search.match(/^\?(lang=en(-gb)?$|ref_src=)/)) window.location = window.location.href.split('?')[0];
+	
+	// Hotkey for search box
+	$('body').on('keydown', event => {
+		var oe = event.originalEvent;
+		if (oe.shiftKey && oe.altKey && !oe.ctrlKey && oe.key == 'F') {
+			$('#search-query').focus().select();
+			event.preventDefault();
+		}
+	});
 	
 });
