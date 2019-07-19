30afd5beadc4205cdd4a448cb09e1caeb8507a27
sjorford <sjorford@gmail.com>
2019-07-19 13:26:30 +0100

    noConflict

--- getAnchors.user.js
+++ getAnchors.user.js
@@ -2,16 +2,18 @@
 // @name           getAnchors
 // @namespace      sjorford@gmail.com
 // @author         Stuart Orford
-// @version        1
+// @version        2019.07.19.1
 // @include        *
 // @grant          none
 // @require        https://code.jquery.com/jquery-3.4.1.min.js
 // @require        https://raw.githubusercontent.com/sjorford/js/master/getSelectedNodes.js
 // ==/UserScript==
 
+var jQuery = $.noConflict();
+
 window.getAnchors = getAnchors;
 window.getSelectedNodes = getSelectedNodes;
 
 function getAnchors() {
-	return $(getSelectedNodes()).filter('a');
+	return jQuery(getSelectedNodes()).filter('a');
 }
