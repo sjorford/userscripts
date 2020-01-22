288c885ef52aa2b6719daefca1c8c65320164832
sjorford <sjorford@gmail.com>
2020-01-22 18:58:40 +0000

    Create wikipedia-extract-goals.user.js
    slightly broken

---
+++ wikipedia-extract-goals.user.js
@@ -0,0 +1,64 @@
+﻿// ==UserScript==
+// @name         Wikipedia extract goals
+// @namespace    sjorford@gmail.com
+// @version      2020.01.20.0
+// @author       Stuart Orford
+// @match        https://en.wikipedia.org/wiki/2019%E2%80%9320_Liverpool_F.C._season
+// @grant        none
+// @require      https://code.jquery.com/jquery-3.4.1.min.js
+// @require      https://raw.githubusercontent.com/sjorford/js/master/sjo-jq.js
+// ==/UserScript==
+
+$(function() {
+	
+	$(`<style>
+	.sjo-wrapper {
+		position: fixed;
+		width: 100%;
+		height: 200px;
+		bottom: 0px;
+		left: 0px;
+		background-color: white;
+		font-size: 9pt;
+		z-index: 9999;
+		overflow-y: scroll;
+		border-top: 1px solid black;
+	}
+	</style>`).appendTo('head');
+	
+	var table = $('<table class="sjo-table"></table>')
+		.appendTo('body').wrap('<div class="sjo-wrapper"></div>')
+		.click(event => table.selectRange());
+	
+	$('img[alt="Goal"]').each((i,e) => {
+		
+		var img = $(e);
+		var cell = img.closest('td');
+		var matchCells = cell.closest('tr').prev('tr').find('td');
+		
+		var team = matchCells.eq(cell[0].cellIndex).text();
+		var date = matchCells.eq(0).find('.dtstart ').text();
+		var competition = matchCells.eq(0).find('small').text();
+		
+		var name = img.prevUntil('br', 'a').attr('title').replace(/\(.*\)/, '').trim();
+		var times = img.nextUntil('br', 'small');
+		
+		times.each((i,e) => {
+			
+			var time, og, pen;
+			[,time,,,og,pen] = $(e).text().match(/(\d+)'(\s+\(((o.g.)|(pen.))\))?/);
+			
+			var row = $('<tr></tr>').appendTo(table);
+			$('<td></td>').text(date).appendTo(row);
+			$('<td></td>').text(competition).appendTo(row);
+			$('<td></td>').text(team).appendTo(row);
+			$('<td></td>').text(name).appendTo(row);
+			$('<td></td>').text(time).appendTo(row);
+			$('<td></td>').text(og).appendTo(row);
+			$('<td></td>').text(pen).appendTo(row);
+			
+		});
+		
+	});
+	
+});
