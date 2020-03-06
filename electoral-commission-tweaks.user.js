de00edb413df14b7a2309aa601661af122bfb143
sjorford <sjorford@gmail.com>
2020-03-06 09:06:18 +0000

    trim panels and stop scrolling

---
+++ electoral-commission-tweaks.user.js
@@ -0,0 +1,20 @@
+﻿// ==UserScript==
+// @name         Electoral Commission tweaks
+// @namespace    sjorford@gmail.com
+// @version      2020.03.06.0
+// @author       Stuart Orford
+// @match        http://search.electoralcommission.org.uk/Search/Registrations?*
+// @grant        none
+// @require      https://code.jquery.com/jquery-3.4.1.min.js
+// ==/UserScript==
+
+$(function() {
+	
+	$('#panelCommon, #panelCriteria, .panel-heading').remove();
+	$('.panel-body').unwrap();
+	
+	var filterCols = $('#divRegistrationSearchFilter > form > .row > .col-sm-3');
+	filterCols.eq(2).empty().append(filterCols.eq(1).contents());
+	filterCols.eq(1).append(filterCols.eq(0).find('.control-group').last());
+	
+});
