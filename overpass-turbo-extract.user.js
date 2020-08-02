b7aa386d4d93e40202c024716190d25ca6f91e2a
sjorford <sjorford@gmail.com>
2020-08-02 14:16:46 +0100

    exclude houses

--- overpass-turbo-extract.user.js
+++ overpass-turbo-extract.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Overpass Turbo extract
 // @namespace    sjorford@gmail.com
-// @version      2020.08.02.0
+// @version      2020.08.02.1
 // @author       Stuart Orford
 // @match        https://overpass-turbo.eu/
 // @grant        none
@@ -15,7 +15,7 @@ $(function() {
 	window.sjo = {getCongletonNodes: getCongletonNodes};
 	
 	function getCongletonNodes() {
-		var url = 'https://overpass-api.de/api/interpreter'
+		var url = 'https://overpass-api.de/api/interpreter';
 		var data = {data: '[out:json][timeout:25];\n(\n  node[~\".\"~\".\"](53.14131327000133,-2.2791481018066406,53.186956172412394,-2.148771286010742);\n);\nout body;\n>;\nout skel qt;'};
 		$.post(url, data, parseData)
 	}
@@ -40,35 +40,37 @@ $(function() {
 		var tbody = $('<tbody></tbody>').appendTo(table);
 		var header = $('<tr></tr>').appendTo(thead);
 		
-		var tags = 'name	addr:housename	addr:housenumber	addr:street	addr:city	addr:postcode	phone	website	facebook	shop	amenity	tourism	brand	brand:wikidata	brand:wikipedia	fhrs:id	collection_times	ref	power	upload_tag	place	denomination	religion	traffic_calming	emergency	postal_code	waste	generator:type	generator:source	generator:method	generator:output:electricity	location	bicycle	is_in	barrier	foot	motor_vehicle	parking	access	opening_hours	noexit	uk_postcode_centroid	highway	crossing	crossing_ref	kerb	tactile_paving	direction	local_ref	bench	shelter	alt_name	bus	public_transport	route_ref	wheelchair	stile	ele	wpt_description	wpt_symbol	horse	motorcar	motorcycle	entrance	natural	maxspeed	speed_camera_type	fee	operator	substation	email	opening_hours:covid19	cuisine	payment:cash	steps	information	waterway	contact:facebook	dance:style	dance:teaching	leisure	backrest	capacity	park_ride	supervised	delivery	drive_through	outdoor_seating	smoking	takeaway	dispensing	healthcare	office	historic	inscription	beauty	craft	man_made	memorial	building	club	short_name	healthcare:speciality	brewery	network	railway	ref:crs	wikipedia	wikidata	train	bicycle_parking	covered	vending	communication:mobile_phone	tower:type	material	openbenches:id	construction	defibrillator:location	indoor	product	denotation	start_date	artwork_type	recycling:clothes	recycling_type	self_service	clothes	proposed	bollard	agrarian	official_name	water	sport	source	source:name	source:addr	source:addr:postcode	note	fixme'.split('\t');
-		var tagsNope = ['created_by', 'upload_tag'];
+		var tags = 'name	shop	amenity	craft	tourism	leisure	office	healthcare	emergency	opening_hours	opening_hours:covid19	fee	highway	crossing	crossing_ref	kerb	tactile_paving	addr:housename	addr:housenumber	addr:street	addr:city	addr:postcode	phone	email	website	facebook	operator	brand	brand:wikipedia	brand:wikidata	takeaway	delivery	cuisine	fhrs:id	collection_times	ref	waste	power	generator:type	generator:source	generator:method	generator:output:electricity	location	substation	barrier	stile	traffic_calming	entrance	natural	steps	information	backrest	capacity	outdoor_seating	dispensing	historic	beauty	man_made	club	openbenches:id'.split('\t');
 		
 		$.each(tags, (i,tag) => $('<th></th>').text(tag).appendTo(header));
 		
 		$.each(data.elements, (i,node) => {
+			
 			var found = false;
-			var row;
+			var values = new Array(tags.length);
+			var other = '';
 			
 			$.each(node.tags, (tag,value) => {
-				if (tagsNope.indexOf(tag) < 0 && !(tag.match(/^naptan:/))) {
-					
-					if (!found) {
-						row = $('<tr>' + '<td></td>'.repeat(tags.length) + '</tr>').appendTo(table);
-						found = true;
-					}
-					
-					var col = tags.indexOf(tag);
-					if (col < 0) {
-						tags.push(tag);
-						$('<th></th>').text(tag).appendTo(header);
-						row.append('<td></td>');
-						col = tags.length - 1;
-					}
-					
-					row[0].cells[col].innerText = value.replace(/\s+/g, ' ').trim();
-					
+				
+				if (tag.match(/^(created_by|upload_tag|naptan|source)(:|$)/)) return;
+				if (!tag.match(/^addr:/)) found = true;
+				var cleanValue = value.replace(/\s+/g, ' ').trim();
+				
+				var col = tags.indexOf(tag);
+				if (col >= 0) {
+					values[col] = cleanValue;
+				} else {
+					other += ' [' + tag + '] ' + cleanValue;
 				}
+				
 			});
+			
+			if (found) {
+				values.push(other.trim());
+				var row = $('<tr></tr>').appendTo(table);
+				$.each(values, (i,value) => $('<td></td>').text(value).appendTo(row));
+			}
+			
 		});
 		
 	}
