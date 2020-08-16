9d489d68c0d25f2280a0b3907c984454a0e4f86e
sjorford <sjorford@gmail.com>
2020-08-16 08:30:27 +0100

    https + cleanup

--- legislation-tweaks.user.js
+++ legislation-tweaks.user.js
@@ -2,10 +2,8 @@
 // @name           Legislation.gov.uk tweaks
 // @namespace      sjorford@gmail.com
 // @author         Stuart Orford
-// @version        2020.01.31.0
-// @match          http://www.legislation.gov.uk/*
-// @match          file:///C:/Users/stuarto/Google%20Drive/Personal/Politics/ECOs/*
-// @match          file:///C:/Users/Stuart/Google%20Drive/Personal/Politics/ECOs/*
+// @version        2020.08.16.0
+// @match          https://www.legislation.gov.uk/*
 // @grant          none
 // ==/UserScript==
 
@@ -48,57 +46,5 @@ $(function() {
 		$('a[href*="/uksi/"]').attr('href', (i,href) => href.replace(/\/contents\//, '/'));
 		
 	}
-	
-	/*
-	// Format "tables"
-	$('table .LegText').closest('tr').each((rowIndex, rowElement) => {
-		
-		var row = $(rowElement);
-		var lastRow = row;
-		var cells = row.find('.LegText').closest('td');
-		cells.each((cellIndex, cellElement) => {
-			
-			var paras = $(cellElement).find('p');
-			paras.each((paraIndex, paraElement) => {
-				if (paraElement == null) return;
-				
-				var para = $(paraElement);
-				var newCell = $('<td class="LegTD"></td>').html(para.html().trim());
-				
-				if (cellIndex == 0) {
-					lastRow = $('<tr class="sjo-row"></tr>').insertAfter(lastRow);
-					if (para.find('strong').length > 0) {
-						newCell.attr('colspan', cells.length - cellIndex);
-						lastRow.removeClass('sjo-row');
-					}
-					lastRow.append(newCell);
-				} else {
-					row.nextAll('.sjo-row').eq(paraIndex).append(newCell);
-				}
-				
-			});
-			
-		});
-		
-		row.remove();
-		
-	});
-	
-	// Repeat district names on each row of county ECO
-	var rows = $('.LegTHplain')
-		.filter((index, element) => $(element).closest('tr').find('td, th')
-			.filter((index, element) => $(element).text().trim() != '').length == 1)
-		.add('.LegTD strong, .LegTH strong').closest('tr');
-	rows.closest('table').css({maxWidth: '800px'}).find('colgroup').remove();
-	rows.each((index, element) => {
-		var row = $(element);
-		var district = row.text().trim().replace(/^(In the )?(city|borough|district) of |^In | (City|Borough|District):?$/ig, '');
-		var followingRows = row.parent().is('thead') ? 
-			row.closest('table').find('tbody tr').first().nextUntil(rows, 'tr').andSelf() :
-			row.nextUntil(rows, 'tr');										
-		followingRows.each((index, element) => $('<td class="LegTDplain"></td>').text(district).prependTo(element));
-		row.remove();
-	});
-	*/
 	
 });
