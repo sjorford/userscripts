eb4cbbc562e36058c882c1801c375cf5ab49d984
sjorford <sjorford@gmail.com>
2021-02-13 13:48:35 +0000

    -th

--- gazette-tweaks.user.js
+++ gazette-tweaks.user.js
@@ -1,9 +1,10 @@
 ﻿// ==UserScript==
 // @name         Gazette tweaks
 // @namespace    sjorford@gmail.com
-// @version      2020.02.13.0
+// @version      2020.02.13.1
 // @author       Stuart Orford
 // @match        https://www.thegazette.co.uk/notice/*
+// @match        https://www.thegazette.co.uk/all-notices/*
 // @grant        none
 // ==/UserScript==
 
@@ -13,11 +14,10 @@ $(function() {
 		
 	</style>`).appendTo('head');
 	
-	$('.full-notice .content').html((i,html) => {
+	$('.content', '.full-notice, .feed-item').html((i,html) => {
 		html = html.replace(/\s+/g, ' ').trim();
-		console.log(html);
 		html = html.replace(/(Baron(?:ess)? .+?\b(?: of .+?\b)?)(,| of )/g, '<a href="https://en.wikipedia.org/wiki/$1">$1</a>$2')
-		console.log(html);
+		html = html.replace(/(\d\d?)(st|nd|rd|th)( \w+ \d{4})/g, '$1$3');
 		return html;
 	});
 	
