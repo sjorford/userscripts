ddf999b1676177c3797da08db4ac9e745ccc4df2
sjorford <sjorford@gmail.com>
2021-03-06 10:23:52 +0000

    legislation.gov.uk

--- search-box-shortcut.user.js
+++ search-box-shortcut.user.js
@@ -2,9 +2,10 @@
 // @name           Search box shortcut
 // @namespace      sjorford@gmail.com
 // @author         Stuart Orford
-// @version        2020.04.13.0
+// @version        2021.03.06.0
 // @match          https://emojipedia.org/*
 // @match          https://gta.fandom.com/*
+// @match          https://www.legislation.gov.uk/*
 // @grant          none
 // @require        https://code.jquery.com/jquery-3.4.1.min.js
 // ==/UserScript==
@@ -14,7 +15,7 @@ $(function() {
 	$('body').on('keydown', event => {
 		var oe = event.originalEvent;
 		if (oe.key == 'F' && oe.shiftKey && oe.altKey && !oe.ctrlKey) {
-			$('[name=q], [name=query]').first().focus().select();
+			$('[name=q], [name=query], #contentSearch #title').first().focus().select();
 			event.preventDefault();
 		}
 	});
