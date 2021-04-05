2c548ff803d7c6079fa3007e6c0db968de562488
sjorford <sjorford@gmail.com>
2021-04-05 10:19:35 +0100

    color bars

---
+++ leap-tweaks.user.js
@@ -0,0 +1,30 @@
+﻿// ==UserScript==
+// @name           LEAP tweaks
+// @namespace      sjorford@gmail.com
+// @version        2021.04.05.0
+// @author         Stuart Orford
+// @match          https://www.andrewteale.me.uk/leap/results/*
+// @grant          none
+// @require        https://code.jquery.com/jquery-3.4.1.min.js
+// ==/UserScript==
+
+(function($) {
+$(function() {
+	
+	$(`<style>
+		.nav_thiscouncil {border-left: 1px solid black;}
+		.nav_thiscouncil li {
+			background-color: white !important; 
+			border-left: 8px solid darkgray;
+			padding: 0 3px;
+		}
+		.nav_thiscouncil li a {
+			text-decoration: none;
+		}
+		.C   {border-left-color: blue !important;}
+		.Lab {border-left-color: red  !important;}
+		.LD  {border-left-color: gold !important;}
+	</style>`).appendTo('head');
+	
+});
+})(jQuery.noConflict());
