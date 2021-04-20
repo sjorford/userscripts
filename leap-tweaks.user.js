658d7f6546dca03a9cfc4f706b46b3d406f2ad48
sjorford <sjorford@gmail.com>
2021-04-20 07:23:45 +0100

    format white text as black

--- leap-tweaks.user.js
+++ leap-tweaks.user.js
@@ -1,8 +1,9 @@
 ﻿// ==UserScript==
 // @name           LEAP tweaks
 // @namespace      sjorford@gmail.com
-// @version        2021.04.05.0
+// @version        2021.04.20.0
 // @author         Stuart Orford
+// @match          http://www.andrewteale.me.uk/leap/results/*
 // @match          https://www.andrewteale.me.uk/leap/results/*
 // @grant          none
 // @require        https://code.jquery.com/jquery-3.4.1.min.js
@@ -20,6 +21,7 @@ $(function() {
 		}
 		.nav_thiscouncil li a {
 			text-decoration: none;
+			color: black !important;
 		}
 		.C   {border-left-color: blue !important;}
 		.Lab {border-left-color: red  !important;}
