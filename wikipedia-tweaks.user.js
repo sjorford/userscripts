d8f5f5b37139e93c588afe3af1c4918368faccc7
sjorford <sjorford@gmail.com>
2021-06-28 10:26:00 +0100

    cut old code

--- wikipedia-tweaks.user.js
+++ wikipedia-tweaks.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @id             wikipedia-tweaks@wikipedia.org@sjorford@gmail.com
 // @name           Wikipedia tweaks
-// @version        2021.05.30.0
+// @version        2021.06.28.0
 // @namespace      sjorford@gmail.com
 // @author         Stuart Orford
 // @include        https://en.wikipedia.org/*
@@ -16,27 +16,10 @@ $(function() {
 		
 	$(`<style>
 		
-		.flagicon {position: relative;}
-		.flagicon.sjo-flagicon-ENG::before {
-			content: "ENG";
-			position: absolute;
-			top: -8px;
-			width: 23px;
-			text-align: center;
-			font-size: 5pt;
-			font-weight: bold;
-		}
-		
-		.sjo-titlelinks {font-size: small;}
-		.sjo-titlelinks a {padding: 0 0.5em;}
-		.sjo-titlelinks span::before {content: "•";}
-		.sjo-titlelinks span:first-of-type::before {content: ""; padding-left: 2rem;}
-		
-		th > div.plainlinks.hlist.navbar.mini {display: none;}
-		
+		/* th > div.plainlinks.hlist.navbar.mini {display: none;} */
 		#pt-userpage, #pt-mytalk, #pt-sandbox, #pt-mycontris, #pt-watchlist {display: none;}
-		#mw-page-header-links, .mw-editsection {display: none;}
-		.navbar-mini {display: none !important;}
+		/* #mw-page-header-links, .mw-editsection {display: none;} */
+		/* .navbar-mini {display: none !important;} */
 		
 		#ca-history a {
 			text-indent: inherit;
@@ -49,12 +32,9 @@ $(function() {
 		
 	</style>`).appendTo('head');
 	
-	//$('.flagicon:has(img[alt="England"])').addClass('sjo-flagicon-ENG');
-	
 	// Hide long references lists
 	$('.reflist').each((index, reflist) => {
-		//console.log('reflist', reflist);
-		if ($('li', reflist).length > 20) {
+		if ($('li', reflist).length > 50) {
 			$(reflist).hide();
 			var wrapper = $('<span class="sjo-reflist-wrapper"></span>').insertBefore(reflist);
 			$(`<a class="sjo-reflist-button sjo-reflist-button-expand sjo-reflist-button-${index}">[Expand]</a>`).appendTo(wrapper);
@@ -74,106 +54,11 @@ $(function() {
 		$('.sjo-reflist-button-collapse').show();
 	});
 	
-	// wtf $('sup').filter((i,e) => !e.innerText.match(/\(|\[/)).html((i,html) => `[${html}]`);
-	
-	/*
-	// Header links
-	var thisURL = window.location.href.split('#')[0];
-	var pageRegex = /\/wiki\/(.*_)([A-Z])$/;
-	var pages = [];
-	var titleMatch = thisURL.match(pageRegex);
-	if (titleMatch) {
-		
-		$('a').each((i,e) => {
-			var linkMatch = e.href.match(pageRegex);
-			if (linkMatch && linkMatch[1] == titleMatch[1] && pages.indexOf(e.href) < 0) {
-				pages.push(e.href);
-			}
-		});
-		
-		console.log(pages);
-		if (pages.length > 0) {
-			var wrapper = $('<span class="sjo-titlelinks"> </span>').appendTo('#firstHeading');
-			$.each(pages.sort(), (i,e) => {
-				$(`<span><a href="${e}">${e.match(pageRegex)[2]}</a></span>`).appendTo(wrapper);
-			});
-		}
-		
-	}
-	
-	pageRegex = /\/en\.wikipedia\.org\/wiki\/(\d+%E2%80%93\d+)_(.*)$/;
-	pages = [thisURL];
-	titleMatch = thisURL.match(pageRegex);
-	if (titleMatch) {
-		
-		$('a').each((i,e) => {
-			var linkMatch = e.href.match(pageRegex);
-			if (linkMatch && linkMatch[2] == titleMatch[2] && pages.indexOf(e.href) < 0) {
-				pages.push(e.href);
-			}
-		});
-		
-		if (pages.length > 0) {
-			wrapper = $('<span class="sjo-titlelinks"> </span>').appendTo('#firstHeading');
-			pages = pages.sort();
-			var pos = pages.indexOf(thisURL);
-			console.log(pages, pos);
-			pages = [pages[pos - 1], pages[pos + 1]];
-			$.each(pages, (i,e) => {
-				if (e) $(`<span><a href="${e}">${decodeURIComponent(e.match(pageRegex)[1])}</a></span>`).appendTo(wrapper);
-			});
-		}
-		
-	}
-	*/
-	
 	var wikidataLink = $('#t-wikibase a');
 	var wikidataID;
 	if (wikidataLink.length > 0) {
 		wikidataID = wikidataLink.attr('href').match(/Q\d+/)[0];
 		wikidataLink.text('Wikidata ' + wikidataID);
-	}
-	
-	/*
-	if (window.location.href == 'https://en.wikipedia.org/wiki/User:DumbBOT/ProdSummary') {
-		$('td').filter((i,e) => e.innerText.trim() == "Fails WP:NFOOTY as the RPFL is now not a WP:FPL league.").closest('tr').hide();
-	}
-	*/
-	
-	//lords();
-	
-	function lords() {
-		
-		var data = {};
-		
-		var pageText;
-		var infobox = $('.infobox').first();
-		if (infobox.length > 0) {
-			if (infobox.parent().is('.mw-parser-output')) {
-				pageText = $('.infobox').nextAll().text();
-			} else {
-				pageText = $('.infobox').parentsUntil('.mw-parser-output').last().nextAll().text();
-			}
-		} else {
-			pageText = $('#mw-content-text').text();
-		}
-		pageText = pageText.replace(/\s+/g, ' ').trim();
-		
-		var pageName = $('#firstHeading').text();
-		[,data.name,data.title] = pageName.match(/^([^,(]+)(?:, ((?:\d?(?:1st|2nd|3rd|\dth) )?(?:Baron|Viscount|Earl|Countess|Marquess|Duke|Duchess)[- 'A-Za-z]+))?( \()?/);
-		if (!data.title) {
-			data.title = pageText.match(/((?:\d?(?:1st|2nd|3rd|\dth) )?(?:Baron|Viscount|Earl|Countess|Marquess|Duke|Duchess)[- 'A-Za-z]+[a-z])/)[1];
-		}
-		
-		[,data.dob,data.dod] = pageText.match(/(?:b(?:\.|orn) |\([^\)]*?)(\d\d? \w+ \d{4})(?:[^\)]+?(\d\d? \w+ \d{4}))?/);
-		
-		data.wikidata = wikidataID;
-		
-		var output = $('<textarea style=""></textarea>')
-			.text([data.name, data.title, data.dob, data.dod, data.wikidata].join('\t'))
-			.prependTo('#mw-content-text')
-			.click((event) => event.target.select());
-		
 	}
 	
 });
