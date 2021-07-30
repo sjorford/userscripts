6fe272f4d7c40b3c7042fd6c6bf34139825965ee
sjorford <sjorford@gmail.com>
2021-07-30 12:13:46 +0100

    sort word bank

--- duolingo-tweaks.user.js
+++ duolingo-tweaks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Duolingo tweaks
 // @namespace    sjorford@gmail.com
-// @version      2021.07.27.0
+// @version      2021.07.29.0
 // @author       Stuart Orford
 // @match        https://www.duolingo.com/*
 // @grant        none
@@ -15,13 +15,49 @@ $(function() {
 		.sjo-league-table {height: auto !important;}
 	</style>`).appendTo('head');
 	
-	window.setInterval(bonk, 1000);
+	var timerLeagueTable = window.setInterval(expandLeagueTable, 1000);
 	
-	function bonk() {
+	function expandLeagueTable() {
 		$('img[src$="/images/leagues/icon_info.svg"]')
 			.closest('div:has(hr)')
 			.find('hr').next('div')
 			.addClass('sjo-league-table');
+	}
+	
+	var delayWordBank = 100;
+	var timerWordBank = window.setInterval(sortWordBank, delayWordBank);
+	var noWordBank = 0;
+	
+	function sortWordBank() {
+		
+		var wordBank = $('[data-test="word-bank"]').not('.sjo-sorted');
+		if (wordBank.length == 0) {
+			noWordBank++;
+			if (noWordBank >= 300 && delayWordBank == 100) {
+				window.clearInterval(timerWordBank);
+				delayWordBank = 1000;
+				timerWordBank = window.setInterval(sortWordBank, delayWordBank);
+			}
+			return;
+		} else {
+			if (delayWordBank > 100) {
+				window.clearInterval(timerWordBank);
+				delayWordBank = 100;
+				timerWordBank = window.setInterval(sortWordBank, delayWordBank);
+				noWordBank = 0;
+			}
+		}
+		
+		wordBank.addClass('sjo-sorted');
+		var sortedWords = wordBank.children('div').toArray().sort((a,b) => {
+			var textA = a.innerText.toLowerCase().trim();
+			var textB = b.innerText.toLowerCase().trim();
+			var sort = (textA > textB) ? 1 : (textA < textB) ? -1 : 0;
+			return sort;
+		});
+		
+		wordBank.append(sortedWords);
+		
 	}
 	
 });
