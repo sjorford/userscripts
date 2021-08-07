ec64517a2744c98e729a89b094124abd9650ec4b
sjorford <sjorford@gmail.com>
2021-08-07 06:05:55 +0100

    Update duolingo-tweaks.user.js

--- duolingo-tweaks.user.js
+++ duolingo-tweaks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Duolingo tweaks
 // @namespace    sjorford@gmail.com
-// @version      2021.07.30.0
+// @version      2021.08.01.0
 // @author       Stuart Orford
 // @match        https://www.duolingo.com/*
 // @grant        none
@@ -24,39 +24,41 @@ $(function() {
 			.addClass('sjo-league-table');
 	}
 	
-	var delayWordBank = 100;
-	var timerWordBank = window.setInterval(sortWordBank, delayWordBank);
-	var noWordBank = 0;
+	var timerWordBank = null;
+	var longDelay = true;
+	var failureCount = 0;
+	resetTimer(true);
+	
+	function resetTimer(_longDelay) {
+		longDelay = _longDelay;
+		window.clearInterval(timerWordBank);
+		return window.setInterval(sortWordBank, longDelay ? 1000 : 100);
+		failureCount = 0;
+	}
 	
 	function sortWordBank() {
 		
 		var wordBank = $('[data-test="word-bank"]').not('.sjo-sorted');
 		if (wordBank.length == 0) {
-			noWordBank++;
-			if (noWordBank >= 3000 && delayWordBank == 100) {
-				window.clearInterval(timerWordBank);
-				delayWordBank = 1000;
-				timerWordBank = window.setInterval(sortWordBank, delayWordBank);
+			failureCount++;
+			if (!longDelay & failureCount >= 3000) {
+				resetTimer(true);
 			}
 			return;
-		} else {
-			if (delayWordBank > 100) {
-				window.clearInterval(timerWordBank);
-				delayWordBank = 100;
-				timerWordBank = window.setInterval(sortWordBank, delayWordBank);
-				noWordBank = 0;
-			}
 		}
 		
-		wordBank.addClass('sjo-sorted');
+		failureCount = 0;
+		if (longDelay) {
+			resetTimer(false);
+		}
+		
 		var sortedWords = wordBank.children('div').toArray().sort((a,b) => {
 			var textA = a.innerText.toLowerCase().trim();
 			var textB = b.innerText.toLowerCase().trim();
 			var sort = (textA > textB) ? 1 : (textA < textB) ? -1 : 0;
 			return sort;
 		});
-		
-		wordBank.append(sortedWords);
+		wordBank.append(sortedWords).addClass('sjo-sorted');
 		
 	}
 	
