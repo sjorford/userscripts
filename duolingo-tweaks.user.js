7c2bcf63c8d7691215bba7450c27a367511dc66f
sjorford <sjorford@gmail.com>
2021-08-10 10:48:33 +0100

    log history

--- duolingo-tweaks.user.js
+++ duolingo-tweaks.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         Duolingo tweaks
 // @namespace    sjorford@gmail.com
-// @version      2021.08.01.0
+// @version      2021.08.10.0
 // @author       Stuart Orford
 // @match        https://www.duolingo.com/*
 // @grant        none
 // @require      https://code.jquery.com/jquery-3.4.1.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.js
 // ==/UserScript==
 
 (function($) {
@@ -13,8 +14,12 @@ $(function() {
 	
 	$(`<style>
 		.sjo-league-table {height: auto !important;}
+		.sjo-history-button {position: fixed; top: 1em; right: 1em; z-index: 200;}
+		.blocker {z-index: 201 !important;}
+		.modal   {z-index: 202 !important;}
 	</style>`).appendTo('head');
 	
+	// Expand league table to full height
 	var timerLeagueTable = window.setInterval(expandLeagueTable, 1000);
 	
 	function expandLeagueTable() {
@@ -24,6 +29,7 @@ $(function() {
 			.addClass('sjo-league-table');
 	}
 	
+	// Sort word bank alphabetically
 	var timerWordBank = null;
 	var longDelay = true;
 	var failureCount = 0;
@@ -59,6 +65,44 @@ $(function() {
 			return sort;
 		});
 		wordBank.append(sortedWords).addClass('sjo-sorted');
+		
+	}
+	
+	// Show history in a modal dialog
+	$('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.css">').appendTo('head');
+	
+	var historyTimer = window.setInterval(updateHistory, 200);
+	var historyButton = $('<a class="sjo-history-button" href="#sjo-history-modal" rel="modal:open">History</a>').appendTo('body');
+	var historyModal = $('<div id="sjo-history-modal" class="modal"></div>').appendTo('body');
+	
+	function updateHistory() {
+		
+		if (!(window.location.href.match(/\/skill\//))) {
+			historyButton.hide();
+			historyModal.empty();
+			return;
+		}
+		
+		historyButton.show();
+		
+		var hints = $('[data-test="hint-sentence"]').not('.sjo-logged').parent().children().not('label');
+		if (hints.length > 0) {
+			var text = hints.toArray().map(e => {
+				if (e.getAttribute('data-test') == 'hint-sentence') return e.innerText;
+				if (e.innerText.length > 0) return e.innerText + ' ';
+				return '_____';
+			}).join('');
+			$('<p></p>').text(text).appendTo(historyModal);
+			hints.addClass('sjo-logged');
+		}
+		
+		$('[data-test="challenge-judge-text"]').not('.sjo-logged').each((i,e) => {
+			$('<p></p>').text(e.innerText).appendTo(historyModal);
+		}).addClass('sjo-logged');
+		
+		$('[data-test="blame blame-correct"], [data-test="blame blame-incorrect"]').find('h2 + div').not('.sjo-logged').each((i,e) => {
+			$('<p></p>').text(e.innerText).appendTo(historyModal);
+		}).addClass('sjo-logged');
 		
 	}
 	
