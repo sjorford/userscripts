816b1f4ca600197cee09fac36132bbbbb9b512e6
sjorford <sjorford@gmail.com>
2021-08-22 10:54:00 +0100

    Create britain-elects-tweaks.user.js

---
+++ britain-elects-tweaks.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name           Britain Elects tweaks
+// @namespace      sjorford@gmail.com
+// @version        2021.08.22.0
+// @author         Stuart Orford
+// @match          http://www.britainelects.com/author/andrewteale/*
+// @grant          none
+// ==/UserScript==
+
+(function($) {
+$(function() {
+	
+	$(`<style>
+		.sjo-links a {margin-right: 2em;}
+	</style>`).appendTo('head');
+	
+	var baseUrl = window.location.href;
+	var pageNo = 1;
+	var urlMatch = window.location.href.match(/^(.+?\/)page\/(\d+)/);
+	if (urlMatch) {
+		baseUrl = urlMatch[1];
+		pageNo = urlMatch[2] - 0;
+	}
+	
+	var wrapper = $('<div class="sjo-links"></div>').insertAfter('.author-profile');
+	
+	if (pageNo > 1)
+		$(`<a href="${baseUrl}page/${pageNo - 1}">Previous page</a>`).appendTo(wrapper);
+		$(`<a href="${baseUrl}page/${pageNo + 1}">Next page</a>`).appendTo(wrapper);
+	
+});
+})(jQuery.noConflict());
