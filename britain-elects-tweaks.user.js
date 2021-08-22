c9b6874e43b7fd5817dd72ab6c1427d599ca26d8
sjorford <sjorford@gmail.com>
2021-08-22 18:02:16 +0100

    move links to bottom

--- britain-elects-tweaks.user.js
+++ britain-elects-tweaks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Britain Elects tweaks
 // @namespace      sjorford@gmail.com
-// @version        2021.08.22.0
+// @version        2021.08.22.1
 // @author         Stuart Orford
 // @match          http://www.britainelects.com/author/andrewteale/*
 // @grant          none
@@ -11,6 +11,7 @@
 $(function() {
 	
 	$(`<style>
+		.sjo-links {padding: 2em;}
 		.sjo-links a {margin-right: 2em;}
 	</style>`).appendTo('head');
 	
@@ -22,7 +23,7 @@ $(function() {
 		pageNo = urlMatch[2] - 0;
 	}
 	
-	var wrapper = $('<div class="sjo-links"></div>').insertAfter('.author-profile');
+	var wrapper = $('<div class="sjo-links"></div>').insertAfter('.page-body');
 	
 	if (pageNo > 1)
 		$(`<a href="${baseUrl}page/${pageNo - 1}">Previous page</a>`).appendTo(wrapper);
