0c3c846a0df6df5a727a17bf55ce625eac5c0eae
sjorford <sjorford@gmail.com>
2021-08-30 18:16:47 +0100

    Update david-boothroyd-tweaks.user.js

--- david-boothroyd-tweaks.user.js
+++ david-boothroyd-tweaks.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name           David Boothroyd tweaks
 // @namespace      sjorford@gmail.com
-// @version        2021.08.30.0
+// @version        2021.08.30.2
 // @author         Stuart Orford
 // @match          https://www.localcouncils.co.uk/*
 // @grant          none
@@ -14,19 +14,22 @@ $(function() {
 		
 	</style>`).appendTo('head');
 	
-	var list = $('h2:contains("Results") + ul');
+	var list = $('h2:contains("Result") + ul');
 	list.find('li').each((i,e) => {
-		var date = e.innerText.match(/\d{1,2} [JFMASOND][a-z]+( \d{4})?/)[0];
-		var subhead = $('.sjo-subhead').filter((i,e) => e.innerText == date);
-		if (subhead.length == 0) {
-			subhead = $('<h3 class="sjo-subhead"></h3>').text(date).insertAfter(list).after('<ul></ul>');
+		
+		e.innerHTML = e.innerHTML.replace(/\bin (.*?) (ward|division)\b/, 'in <strong>$1</strong> $2');
+		
+		var dateMatch = e.innerText.match(/\d{1,2} [JFMASOND][a-z]+( \d{4})?/);
+		if (dateMatch) {
+			var date = dateMatch[0];
+			var subhead = $('.sjo-subhead').filter((i,e) => e.innerText == date);
+			if (subhead.length == 0) {
+				subhead = $('<h3 class="sjo-subhead"></h3>').text(date).insertAfter(list).after('<ul></ul>');
+			}
+			subhead.next('ul').append(e);
 		}
-		subhead.next('ul').append(e);
+		
 	});
-	
-	
-	
-	
 	
 });
 })(jQuery.noConflict());
