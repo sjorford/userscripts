7fdca236f10f1ca44450f828278346b5744b55ac
sjorford <sjorford@gmail.com>
2021-10-31 08:05:50 +0000

    expand crossword clues

--- guardian-tweaks.user.js
+++ guardian-tweaks.user.js
@@ -4,7 +4,7 @@
 // @namespace   sjorford@gmail.com
 // @include     http://www.theguardian.com/*
 // @include     https://www.theguardian.com/*
-// @version     2018.05.09.0
+// @version     2021.10.31.0
 // @grant       none
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
 // ==/UserScript==
@@ -12,15 +12,17 @@
 $(function() {
 	
 	$(`<style>
+		
 		#comments {display: none;}
 		.live-blog__sticky-components-container.affix {position: static;}
 		.weather {display: none;}
+		
+		@media (min-width: 81.25em) {
+			.crossword__spacer--ad {margin-right: 0;}
+			.crossword__clues {display: block; column-count: 3;}
+			.crossword__clues--across, .crossword__clues--down {display: block; padding-left: 0;}
+		}
+		
 	</style>`).appendTo('head');
-	
-	$('.fc-item__byline, .fc-item__kicker').filter(`
-		:contains("Suzanne Moore"), 
-		:contains("Zoe Williams"), 
-		:contains("Germaine Greer"), 
-		:contains("Tim Dowling")`).closest('.fc-item').hide();
 	
 });
