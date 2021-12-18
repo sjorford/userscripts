b2741657d10c730ef5e689ebb8ccef6fe25b3408
sjorford <sjorford@gmail.com>
2021-12-18 07:45:50 +0000

    fix jumping

--- guardian-tweaks.user.js
+++ guardian-tweaks.user.js
@@ -4,7 +4,7 @@
 // @namespace   sjorford@gmail.com
 // @include     http://www.theguardian.com/*
 // @include     https://www.theguardian.com/*
-// @version     2021.12.18.4
+// @version     2021.12.18.5
 // @grant       none
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
 // ==/UserScript==
@@ -21,6 +21,7 @@ $(function() {
 			.crossword__spacer--ad {margin-right: 0;}
 			.crossword__clues {display: block; column-count: 3;}
 			.crossword__clues--across, .crossword__clues--down {display: block; padding-left: 0;}
+			.crossword__clues-list {margin-bottom: 0 !important;}
 		}
 		
 	</style>`).appendTo('head');
