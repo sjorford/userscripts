d5d4e15dec4207b96cc68c881aa86be7b01ab861
sjorford <sjorford@gmail.com>
2021-12-18 06:24:28 +0000

    Christmas crossword

--- guardian-tweaks.user.js
+++ guardian-tweaks.user.js
@@ -4,7 +4,7 @@
 // @namespace   sjorford@gmail.com
 // @include     http://www.theguardian.com/*
 // @include     https://www.theguardian.com/*
-// @version     2021.12.18.0
+// @version     2021.12.18.1
 // @grant       none
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
 // ==/UserScript==
@@ -23,10 +23,26 @@ $(function() {
 			.crossword__clues--across, .crossword__clues--down {display: block; padding-left: 0;}
 		}
 		
-		/* Christmas crossword 23x23 */
+	</style>`).appendTo('head');
+	
+	window.setTimeout(() => {
+	
+	// Christmas crossword 23x23
+	var cluesAcross  = $('.crossword__clues--across .crossword__clues-list');
+	var cluesDown    = $('.crossword__clues--down   .crossword__clues-list').hide();
+	var cluesHeaders = $('.crossword__clues .crossword__clues-header').insertBefore('.crossword__clues')
+							.wrapAll('<div class="sjo-xword-header-wrapper"></div>');
+	cluesHeaders.click(() => cluesAcross.toggle() && cluesDown.toggle() && cluesHeaders.toggleClass('sjo-xword-header-active'))
+	cluesHeaders.first().addClass('sjo-xword-header-active');
+		
+	$(`<style>
 		.crossword__container--prize .crossword__hidden-input-wrapper {width: 4%; height: 4%;}
 		.crossword__hidden-input {position: relative; top: -2px;}
-		
+		.sjo-xword-header-wrapper {padding-left: 1.25rem; column-count: 3;}
+		.sjo-xword-header-wrapper .crossword__clues-header {cursor: pointer;}
+		.sjo-xword-header-active {background-color: #fff7b2;}
 	</style>`).appendTo('head');
+	
+	}, 1000);
 	
 });
