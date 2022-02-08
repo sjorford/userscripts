f4414f897b206e3a7d681ac980c3203d5ad56230
sjorford <sjorford@gmail.com>
2022-02-08 17:06:30 +0000

    Create wikidata-tweaks.user.js

---
+++ wikidata-tweaks.user.js
@@ -0,0 +1,22 @@
+﻿// ==UserScript==
+// @name           Wikidata tweaks
+// @namespace      sjorford@gmail.com
+// @version        2022.02.08.0
+// @author         Stuart Orford
+// @match          https://www.wikidata.org/wiki/Q*
+// @grant          none
+// ==/UserScript==
+
+//(function($) {
+jQuery(function() {
+	
+	console.log('Wikidata tweaks');
+	
+	$(`<style>
+		
+	</style>`).appendTo('head');
+	
+	$('div.wikibase-sitelinkgroupview:contains("(0 entries)")').hide();
+	
+});
+//})(jQuery.noConflict());
