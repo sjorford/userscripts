e286a9a1e55a1762133abbab2e833ecae2049792
sjorford <sjorford@gmail.com>
2022-07-05 08:47:19 +0100

    Create symantec-cloud-tweaks.user.js

---
+++ symantec-cloud-tweaks.user.js
@@ -0,0 +1,20 @@
+﻿// ==UserScript==
+// @name           Symantec.cloud tweaks
+// @namespace      sjorford@gmail.com
+// @version        2022.07.05.0
+// @author         Stuart Orford
+// @match          https://clients.messagelabs.com/Tools/*
+// @grant          none
+// @require        https://code.jquery.com/jquery-3.4.1.min.js
+// ==/UserScript==
+
+(function($) {
+$(function() {
+	
+	$(`<style>
+		#body, #wrap, div.round {width: auto;}
+		#header-content, #body-main-content {padding-right: 10px;}
+	</style>`).appendTo('head');
+	
+});
+})(jQuery);
