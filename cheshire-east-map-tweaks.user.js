77d363f0581ef24c614c3e7d835d4b0d654f9d1a
sjorford <sjorford@gmail.com>
2023-01-25 07:39:24 +0000

    shrink popups

--- cheshire-east-map-tweaks.user.js
+++ cheshire-east-map-tweaks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Cheshire East map tweaks
 // @namespace      sjorford@gmail.com
-// @version        2022.07.18.0
+// @version        2023.01.25.0
 // @author         Stuart Orford
 // @match          https://maps.cheshireeast.gov.uk/ce/webmapping
 // @grant          none
@@ -11,9 +11,12 @@
 $(function() {
 	
 	$(`<style>
-		#popup-content td {
-			padding: 0.25em !important;
-		}
+		.ol-viewport #popup-content {min-width: 300px !important;}
+		.ol-viewport #popup-content td {padding: 0.25em !important;}
+		.ol-viewport .panel-body {display: none !important;}
+		.ol-viewport .table {margin: 0 !important;}
+		.ol-viewport .ol-popup {padding: 15px 0 0 0 !important;}
+		.ol-viewport [id^="identpopup_"] {padding: 0 !important;}
 	</style>`).appendTo('head');
 	
 });
