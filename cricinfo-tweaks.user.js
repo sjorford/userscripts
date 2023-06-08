53121209ac8f540f5b9f60a3026a752dd1ad717c
sjorford <sjorford@gmail.com>
2023-06-08 14:43:50 +0100

    Create cricinfo-tweaks.user.js

---
+++ cricinfo-tweaks.user.js
@@ -0,0 +1,19 @@
+﻿// ==UserScript==
+// @name           Cricinfo tweaks
+// @namespace      sjorford@gmail.com
+// @version        2023.06.08.0
+// @author         Stuart Orford
+// @match          https://www.espncricinfo.com/series/icc-world-test-championship-2021-2023-1268315/australia-vs-india-final-1358412/live-cricket-score
+// @grant          none
+// @require        https://code.jquery.com/jquery-3.4.1.min.js
+// ==/UserScript==
+
+(function($) {
+$(function() {
+	
+	$(`<style>
+		.ds-select-none {user-select: auto;}
+	</style>`).appendTo('head');
+	
+});
+})(jQuery);
