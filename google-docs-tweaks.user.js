10698d687fbe32ccf2e0b744e2ebbce69988e7c6
sjorford <sjorford@gmail.com>
2023-08-14 07:46:22 +0100

    Create google-docs-tweaks.user.js

---
+++ google-docs-tweaks.user.js
@@ -0,0 +1,18 @@
+﻿// ==UserScript==
+// @name        Google docs tweaks
+// @namespace   sjorford@gmail.com
+// @include     https://docs.google.com/spreadsheets*
+// @version     2023.08.14.0
+// @grant       none
+// ==/UserScript==
+
+(function() {
+	
+	var head = document.getElementsByTagName('head')[0];
+	var style = document.createElement('style');
+	style.innerText = `
+		.docos-anchoreddocoview {top: 0px !important; left: 0px !important;}
+	`;
+	head.append(style);
+	
+})();
