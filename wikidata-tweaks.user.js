66f115e576ac3116417c984d59f58fcb8f9e0a93
sjorford <sjorford@gmail.com>
2023-09-23 06:55:48 +0100

    WP link in header

--- wikidata-tweaks.user.js
+++ wikidata-tweaks.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name           Wikidata tweaks
 // @namespace      sjorford@gmail.com
-// @version        2023.09.16.0
+// @version        2023.09.23.0
 // @author         Stuart Orford
 // @match          https://www.wikidata.org/wiki/Q*
 // @grant          none
@@ -16,9 +16,20 @@
 			
 			$(`<style>
 				#P6465 div, #P6465 span {background-color: #ffe65e !important;}
+				#sjo-wikipedia a {
+					background-image: url(//upload.wikimedia.org/wikipedia/commons/thumb/6/63/Wikipedia-logo.png/35px-Wikipedia-logo.png);
+					background-size: 20px;
+					padding-left: 23px;
+				}
 			</style>`).appendTo('head');
 			
 			$('div.wikibase-sitelinkgroupview:contains("(0 entries)")').hide();
+			
+			var enwiki = $('.wikibase-sitelinkview-enwiki a');
+			if (enwiki.length > 0) {
+				$(`<li id="sjo-wikipedia" class="mw-list-item"><a href="${enwiki.attr('href')}" title="${enwiki.text()}"><span>${enwiki.text()}</span></a></li>`)
+					.insertAfter('#ca-talk');
+			}
 			
 		}
 	}, 100);
