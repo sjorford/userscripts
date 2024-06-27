838efd7ed98019e8901aa723e5476f5dbf1a26fb
sjorford <sjorford@gmail.com>
2024-06-27 08:13:27 +0100

    Update leap-tweaks.user.js

--- leap-tweaks.user.js
+++ leap-tweaks.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name           LEAP tweaks
 // @namespace      sjorford@gmail.com
-// @version        2021.04.20.0
+// @version        2024.06.27.0
 // @author         Stuart Orford
 // @match          http://www.andrewteale.me.uk/leap/results/*
 // @match          https://www.andrewteale.me.uk/leap/results/*
@@ -26,7 +26,19 @@ $(function() {
 		.C   {border-left-color: blue !important;}
 		.Lab {border-left-color: red  !important;}
 		.LD  {border-left-color: gold !important;}
+		.sjo-current-page {background-color: gold;}
 	</style>`).appendTo('head');
+	
+	// Navigation
+	var wrapper = $('<div style="margin-top: 1em;"></div>').insertBefore('h1');
+	var current = $('a').filter((i,e) => e.href == window.location.href).closest('li').addClass('sjo-current-page');
+	var prev = current.prev();
+	var next = current.next();
+	var prevURL = prev.find('a').attr('href');
+	var nextURL = next.find('a').attr('href');
+	if (prevURL) wrapper.append(`<a href="${prevURL}">${prev.text()}</a> < `);
+	wrapper.append(`<strong>${current.text()}</strong>`);
+	if (nextURL) wrapper.append(` < <a href="${nextURL}">${next.text()}</a>`);
 	
 });
 })(jQuery.noConflict());
