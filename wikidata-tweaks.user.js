0d9b2800ab7405b18cb28012af4d16a479db9b7a
sjorford <sjorford@gmail.com>
2024-07-06 08:08:13 +0100

    Update wikidata-tweaks.user.js

--- wikidata-tweaks.user.js
+++ wikidata-tweaks.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name           Wikidata tweaks
 // @namespace      sjorford@gmail.com
-// @version        2024.07.06.0
+// @version        2024.07.06.1
 // @author         Stuart Orford
 // @match          https://www.wikidata.org/wiki/Q*
 // @grant          none
@@ -20,6 +20,11 @@
 					background-size: 20px;
 					padding-left: 23px;
 				}
+				#sjo-dc a {
+					background-image: url(https://dc-shared-frontend-assets.s3.eu-west-2.amazonaws.com/images/logo_icon.svg);
+					background-size: 20px;
+					padding-left: 23px;
+				}
 				.sjo-dc-logo {
 					position: absolute; bottom: 10px; left: 10px; height: 50px;
 				}
@@ -30,7 +35,15 @@
 			var enwiki = $('.wikibase-sitelinkview-enwiki a');
 			if (enwiki.length > 0) {
 				$(`<li id="sjo-wikipedia" class="mw-list-item"><a href="${enwiki.attr('href')}" title="${enwiki.text()}"><span>Wikipedia</span></a></li>`)
-					.insertAfter('#ca-talk');
+					.insertBefore('#ca-watch');
+			}
+			
+			var dclinks = $('#P6465 .wikibase-statementview:not(.wb-deprecated) .wikibase-statementview-mainsnak a.wb-external-id');
+			console.log(dclinks);
+			if (dclinks.length == 1) {
+				$(`<li id="sjo-dc" class="mw-list-item"><a href="${dclinks.attr('href')}" title="${dclinks.text()}"><span>Democracy Club</span></a></li>`)
+					.insertBefore('#ca-watch');
+				
 			}
 			
 			$('<img class="sjo-dc-logo" src="https://dc-shared-frontend-assets.s3.eu-west-2.amazonaws.com/images/logo_icon.svg">')
