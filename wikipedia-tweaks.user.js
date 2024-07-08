9c2c345251e4a063dba94eb346e2e2a89c04456d
sjorford <sjorford@gmail.com>
2024-07-08 08:03:19 +0100

    Update wikipedia-tweaks.user.js

--- wikipedia-tweaks.user.js
+++ wikipedia-tweaks.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @id             wikipedia-tweaks@wikipedia.org@sjorford@gmail.com
 // @name           Wikipedia tweaks
-// @version        2023.08.17.0
+// @version        2024.07.08.0
 // @namespace      sjorford@gmail.com
 // @author         Stuart Orford
 // @include        https://en.wikipedia.org/*
@@ -34,6 +34,12 @@ $(function() {
 		.mw-category-group h3 {display: none;}
 		.read-more-container {display: none;}
 		
+		#sjo-wikipedia a {
+			background-image: url(//upload.wikimedia.org/wikipedia/commons/thumb/6/63/Wikipedia-logo.png/35px-Wikipedia-logo.png);
+			background-size: 20px;
+			padding-left: 23px;
+		}
+		
 	</style>`).appendTo('head');
 	
 	// Hide long references lists
@@ -63,6 +69,10 @@ $(function() {
 	if (wikidataLink.length > 0) {
 		wikidataID = wikidataLink.attr('href').match(/Q\d+/)[0];
 		wikidataLink.text('Wikidata ' + wikidataID);
+		
+		$(`<li id="sjo-wikipedia" class="mw-list-item"><a href="${wikidataLink.attr('href')}" title="${wikidataID}"><span>Wikidata</span></a></li>`)
+			.insertBefore('#ca-watch');
+		
 	}
 	
 });
