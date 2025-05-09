21b4acaf7213197f7dea04efb509a2bdca68ad18
sjorford <sjorford@gmail.com>
2025-05-09 10:45:14 +0100

    collapse annotations

--- legislation-tweaks.user.js
+++ legislation-tweaks.user.js
@@ -2,7 +2,7 @@
 // @name           Legislation.gov.uk tweaks
 // @namespace      sjorford@gmail.com
 // @author         Stuart Orford
-// @version        2025.05.09.0
+// @version        2025.05.09.1
 // @match          https://www.legislation.gov.uk/*
 // @grant          none
 // ==/UserScript==
@@ -10,6 +10,16 @@
 $(function() {
 	
 	$(`<style>
+		.sjo-collapsed {display: none;}
 	</style>`).appendTo('head');
+	
+	$('p.LegAnnotationsGroupHeading').each((i,e) => {
+		console.log(i, e);
+		var heading = $(e);
+		var annotations = heading.nextUntil('p').wrapAll(`<div class="sjo-collapsible sjo-collapsed" id="sjo-collapsible-${i}"></div>`);
+		heading.click(event => {
+			$(`#sjo-collapsible-${i}`).toggleClass('sjo-collapsed');
+		});
+	});
 	
 });
