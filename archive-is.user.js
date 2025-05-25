4b8fe44c1f3a2616e627ae3edd208a24e197ce57
sjorford <sjorford@gmail.com>
2025-05-25 20:05:53 +0100

    Create archive-is.user.js

---
+++ archive-is.user.js
@@ -0,0 +1,19 @@
+﻿// ==UserScript==
+// @name           archive.is
+// @namespace      sjorford@gmail.com
+// @version        2025.05.25.0
+// @author         Stuart Orford
+// @match          https://www.ft.com/*
+// @grant          none
+// @require        https://code.jquery.com/jquery-3.4.1.min.js
+// ==/UserScript==
+
+(function($) {
+$(function() {
+	
+	if ($('#charge-button').length > 0) {
+		window.location = `https://archive.is/search/?q=${encodeURIComponent(window.location.href)}`
+	}
+	
+});
+})(jQuery);
