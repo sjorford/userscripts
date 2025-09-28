46b9134976fa11727648353b5783332ea12ca44b
Stuart Orford <sjorford@gmail.com>
2025-09-28 09:28:03 +0100

    Update fantasy-premier-league-extract.user.js

--- fantasy-premier-league-extract.user.js
+++ fantasy-premier-league-extract.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Fantasy Premier League extract
 // @namespace      sjorford@gmail.com
-// @version        2025.09.28.2
+// @version        2025.09.28.3
 // @author         Stuart Orford
 // @match          https://fantasy.premierleague.com/statistics
 // @grant          none
@@ -57,7 +57,7 @@
 					teamsMap[team.id] = team.name;
 				});
 				
-				var maxWeek = Math.max(...data.events.filter(week => week.finished == true).map(week => week.id));
+				var maxWeek = Math.max(...data.events.filter(week => week.finished == true && week.data_checked == true).map(week => week.id));
 				
 				var players = data.elements; //.filter(player => player.total_points != 0);
 				var count = 0;
@@ -69,23 +69,33 @@
 					var player = players.shift();
 					count ++;
 					
-					var outputRow = $('<tr></tr>').appendTo(table);
-					$('<td></td>').appendTo(outputRow).text(player.code);
-					$('<td></td>').appendTo(outputRow).text(player.web_name);
-					$('<td></td>').appendTo(outputRow).text((player.first_name + ' ' + player.second_name).trim());
-					$('<td></td>').appendTo(outputRow).text(teamsMap[player.team]);
-					$('<td></td>').appendTo(outputRow).text(posMap[player.element_type]);
-					$('<td></td>').appendTo(outputRow).text(player.now_cost / 10);
-					$('<td></td>').appendTo(outputRow).text(player.total_points);
-					$('<td></td>').appendTo(outputRow).text(player.news == '' ? '' : '[' + player.news_added.substr(0, 10) + '] ' + player.news);
-					
 					$.get(`/api/element-summary/${player.id}/`, processPlayerHistory);
 					
 					function processPlayerHistory(data) {
 						
+						var weekPoints = [];
+						var totalPoints = 0;
+						
 						for (var week = 1; week <= maxWeek; week++) {
 							var weekData = data.history.filter(history => history.round == week);
-							$('<td></td>').appendTo(outputRow).text(weekData.length == 0 ? '' : weekData[0].total_points);
+							if (weekData.length > 0) {
+								weekPoints[week] = weekData[0].total_points;
+								totalPoints += weekData[0].total_points;
+							}
+						}
+						
+						var outputRow = $('<tr></tr>').appendTo(table);
+						$('<td></td>').appendTo(outputRow).text(player.code);
+						$('<td></td>').appendTo(outputRow).text(player.web_name);
+						$('<td></td>').appendTo(outputRow).text((player.first_name + ' ' + player.second_name).trim());
+						$('<td></td>').appendTo(outputRow).text(teamsMap[player.team]);
+						$('<td></td>').appendTo(outputRow).text(posMap[player.element_type]);
+						$('<td></td>').appendTo(outputRow).text(player.now_cost / 10);
+						$('<td></td>').appendTo(outputRow).text(totalPoints);
+						$('<td></td>').appendTo(outputRow).text(player.news == '' ? '' : '[' + player.news_added.substr(0, 10) + '] ' + player.news);
+						
+						for (var week = 1; week <= maxWeek; week++) {
+							$('<td></td>').appendTo(outputRow).text(weekPoints === undefined ? '' : weekPoints[week]);
 						}
 						
 						//if (count >= 5) return;
