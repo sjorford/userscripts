1154eb87b88c39b70376514b38ea0bdfaa6a75d9
Stuart Orford <sjorford@gmail.com>
2025-09-28 08:36:46 +0100

    Update fantasy-premier-league-extract.user.js

--- fantasy-premier-league-extract.user.js
+++ fantasy-premier-league-extract.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Fantasy Premier League extract
 // @namespace      sjorford@gmail.com
-// @version        2025.09.28.0
+// @version        2025.09.28.2
 // @author         Stuart Orford
 // @match          https://fantasy.premierleague.com/statistics
 // @grant          none
@@ -12,29 +12,12 @@
 (function($) {
 	$(function() {
 		
-		var posMap = ['GK', 'DF', 'MF', 'FW'];
-		var teamsMap = [
-			"Arsenal", 
-			"Aston Villa", 
-			"Bournemouth", 
-			"Brentford", 
-			"Brighton", 
-			"Burnley", 
-			"Chelsea", 
-			"Crystal Palace", 
-			"Everton", 
-			"Fulham", 
-			"Leeds", 
-			"Liverpool", 
-			"Man City", 
-			"Man Utd", 
-			"Newcastle", 
-			"Nott'm Forest", 
-			"Spurs", 
-			"Sunderland", 
-			"West Ham", 
-			"Wolves", 
-		];
+		var posMap = {
+			1: 'GK', 
+			2: 'DF', 
+			3: 'MF', 
+			4: 'FW',
+		};
 		
 		$(`<style>
 			.sjo-wrapper {
@@ -48,6 +31,7 @@
 		var timer = window.setInterval(check, 100);
 		
 		function check() {
+			
 			var reset = $('span:contains("Reset")');
 			if (reset.length == 0) return;
 			window.clearInterval(timer);
@@ -55,50 +39,58 @@
 			var button = $('<button class="u523kl2 u523kl1 u523kl0 u523kl5" data-rac="" type="button" tabindex="0" data-react-aria-pressable="true" aria-label="Extract" id="sjo-extract"><span class="t2sh1k0">Extract</span></button>')
 					.click(extract);
 			reset.closest('div').append(button);
+			
 		}
 		
 		function extract() {
 			
 			var table = $('<table class="sjo-table"></table>')
-			.appendTo('body').wrap('<div class="sjo-wrapper"></div>')
-			.click(event => table.selectRange());
+					.appendTo('body').wrap('<div class="sjo-wrapper"></div>')
+					.click(event => table.selectRange());
 			
 			$.get('/api/bootstrap-static/', processPlayerList);
 			
 			function processPlayerList(data) {
-				var players = data.elements.filter(player => player.total_points != 0);
+				
+				var teamsMap = {};
+				$.each(data.teams, (i,team) => {
+					teamsMap[team.id] = team.name;
+				});
+				
+				var maxWeek = Math.max(...data.events.filter(week => week.finished == true).map(week => week.id));
+				
+				var players = data.elements; //.filter(player => player.total_points != 0);
 				var count = 0;
 				nextPlayer();
 				
 				function nextPlayer() {
+					
 					if (players.length == 0) return;
 					var player = players.shift();
 					count ++;
 					
 					var outputRow = $('<tr></tr>').appendTo(table);
-					$('<td></td>').appendTo(outputRow).text(player.id);
+					$('<td></td>').appendTo(outputRow).text(player.code);
 					$('<td></td>').appendTo(outputRow).text(player.web_name);
 					$('<td></td>').appendTo(outputRow).text((player.first_name + ' ' + player.second_name).trim());
-					$('<td></td>').appendTo(outputRow).text(teamsMap[player.team - 1]);
-					$('<td></td>').appendTo(outputRow).text(posMap[player.element_type - 1]);
+					$('<td></td>').appendTo(outputRow).text(teamsMap[player.team]);
+					$('<td></td>').appendTo(outputRow).text(posMap[player.element_type]);
+					$('<td></td>').appendTo(outputRow).text(player.now_cost / 10);
 					$('<td></td>').appendTo(outputRow).text(player.total_points);
-					$('<td></td>').appendTo(outputRow).text(player.now_cost);
-					
-					if (player.news != '') {
-						$('<td></td>').appendTo(outputRow).text('[' + player.news_added.substr(0, 10) + '] ' + player.news);
-					}
+					$('<td></td>').appendTo(outputRow).text(player.news == '' ? '' : '[' + player.news_added.substr(0, 10) + '] ' + player.news);
 					
 					$.get(`/api/element-summary/${player.id}/`, processPlayerHistory);
 					
 					function processPlayerHistory(data) {
 						
-						var weekPoints = data.history.map(week => week.total_points);
-						$.each(weekPoints, (i,points) => {
-							$('<td></td>').appendTo(outputRow).text(points);
-						});
+						for (var week = 1; week <= maxWeek; week++) {
+							var weekData = data.history.filter(history => history.round == week);
+							$('<td></td>').appendTo(outputRow).text(weekData.length == 0 ? '' : weekData[0].total_points);
+						}
 						
-						//if (count >= 50) return;
+						//if (count >= 5) return;
 						nextPlayer();
+						
 					}
 					
 				}
